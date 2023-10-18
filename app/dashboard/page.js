7f9a135299443138091d426d5c7efb216f2b36dc
Green Gem <chris_waitt@me.com>
2023-10-18 21:38:42 +0100

    Add a new data view for the dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -26,6 +26,12 @@ async function getWorkouts(userId) {
 	return workouts;
 }
 
+function formatDuration(seconds) {
+	const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}m ${remainingSeconds}s`;
+}
+
 export default async function DashboardPage() {
 	const authRequest = auth.handleRequest("GET", context);
 	const session = await authRequest.validate();
@@ -47,7 +53,7 @@ export default async function DashboardPage() {
 			  <div key={workout.id}>
 				<strong>Workout Name:</strong> {workout.name}
 				<strong>Date and Time:</strong> {new Date(workout.date).toLocaleString()}
-				<strong>Duration:</strong> {workout.duration}
+				<strong>Duration:</strong> {formatDuration(workout.duration)}
 				<ul>
 				  {workout.WorkoutLogExercise.map(wle => (
 					<li key={wle.id}>
@@ -67,6 +73,48 @@ export default async function DashboardPage() {
 				<hr className="my-5" />
 			  </div>
 			))}
+			</div>
+
+			<div className="bg-gray-300 p-5 my-10">
+				<h4 className="text-xl font-semibold">BEST SET STYLE</h4>
+				<hr />
+				{workouts.map((workout) => {
+					const totalWeightLifted = workout.WorkoutLogExercise.reduce((acc, wle) => {
+						return acc + wle.SetLog.reduce((acc, set) => acc + set.weight, 0);
+					}, 0);
+
+					return (
+						<div key={workout.id}>
+							<h5>{workout.name}</h5>
+							<p>{formatDuration(workout.duration)} | {totalWeightLifted} KG</p>
+							<table className="min-w-full divide-y divide-gray-200 mt-4">
+								<thead>
+									<tr>
+										<th className="py-2 px-4">EXERCISE</th>
+										<th className="py-2 px-4">BEST SET</th>
+									</tr>
+								</thead>
+								<tbody>
+									{workout.WorkoutLogExercise.map(wle => {
+										// Determine the best set for this exercise
+										const bestSet = wle.SetLog.reduce((best, set) => {
+											return set.weight > best.weight ? set : best;
+										}, { weight: 0, reps: 0 });
+
+										return (
+											<tr key={wle.id}>
+												<td className="py-2 px-4">{wle.Exercise.name}</td>
+												<td className="py-2 px-4">{bestSet.reps} X {bestSet.weight} KG</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</table>
+							<DeleteButton id={workout.id} />
+							<hr className="my-5" />
+						</div>
+					);
+				})}
 			</div>
 		</>
 	);
