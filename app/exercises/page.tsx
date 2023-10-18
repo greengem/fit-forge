8f7cc798c8bc816c0d2074bb544d1e5cc1b8586c
Green Gem <chris_waitt@me.com>
2023-10-18 17:10:56 +0100

    Exercise Display Page - Since we're using App router with Server Components it's best practice to fetch data directly in the component, fetching data from your own api routes / route handlers is bad practice in RSC. - Display data in a simple table for now. - Issues: over 800 results are in this table so i will implement pagination later for efficiency.

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,7 +1,47 @@
+import prisma from '@/db/prisma';
 import PageHeading from '@/components/PageHeading/PageHeading';
 
-export default function ExercisesPage() {
+async function getExercises(){
+  const exercises = await prisma.exercise.findMany();
+  return exercises;
+}
+
+export default async function ExercisesPage() {
+  const exercises = await getExercises()
+
   return (
+    <>
     <PageHeading title="Exercises" />
+    <table className="w-full mt-4 bg-white shadow-md rounded-lg overflow-hidden">
+      <thead className="bg-gray-200">
+        <tr>
+          <th className="px-6 py-3 text-left font-semibold text-gray-600">Name</th>
+          <th className="px-6 py-3 text-left font-semibold text-gray-600">Muscles</th>
+          <th className="px-6 py-3 text-left font-semibold text-gray-600">Level</th>
+          <th className="px-6 py-3 text-left font-semibold text-gray-600">More Info</th>
+        </tr>
+      </thead>
+      <tbody className="divide-y divide-gray-200">
+        {exercises.map((exercise) => (
+          <tr key={exercise.id}>
+            <td className="px-6 py-4 whitespace-nowrap">
+              <div>
+                <p>{exercise.name}</p>
+                <p>{exercise.category}</p>
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+            <div>
+              <p>{exercise.primary_muscles.join(', ')}</p>
+                <p>{exercise.secondary_muscles.join(', ')}</p>
+              </div>
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">{exercise.level}</td>
+            <td className="px-6 py-4 whitespace-nowrap"><button>More Info</button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </>
   )
 }
