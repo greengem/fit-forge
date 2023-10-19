4ebdc0d94840f6c1a71b8b2b1e47e691f72fdea3
Green Gem <chris_waitt@me.com>
2023-10-19 14:03:16 +0100

    Default Theme set to Light

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='dark'>
+    <html lang="en" className='light'>
       <body>
           <Toaster />
           <Providers>
