86105481c87aba2325d899f307c9c17ca27856f2
Green Gem <chris_waitt@me.com>
2023-10-20 14:41:10 +0100

    Removed Bundle Analyser after completing analysis

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  });
-  
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     reactStrictMode: false,
-    // ... you can add other configurations as needed
-  };
-  
-  module.exports = withBundleAnalyzer(nextConfig);
-  
+}
+
+module.exports = nextConfig
