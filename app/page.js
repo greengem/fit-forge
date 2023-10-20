dc901ff8d277ea4ca8071a0ca412dff748d8f66f
Green Gem <chris_waitt@me.com>
2023-10-20 13:43:36 +0100

    Basic Landing / Hero page - needs work

--- app/page.js
+++ app/page.js
@@ -1,18 +1,41 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import PageHeading from '@/components/PageHeading/PageHeading';
+import Link from "next/link"
+import { Button } from "@nextui-org/button"
+import { IconSearch, IconFilePlus, IconNotebook } from "@tabler/icons-react"
 
 export default async function HomePage() {
-  const session = await getServerSession(authOptions);
-
   return (
-    <>
-    <PageHeading title="Landing Page" />
-    <p>name {session?.user?.name}</p>
-    <p>email {session?.user?.email}</p>
-    <p>image {session?.user?.image}</p>
-    <p>id {session?.user?.id}</p>
-    <p>userId {session?.user?.userId}</p>
-    </>
+    <div className="relative h-screen bg-gradient-to-b from-teal-500 to-blue-600">
+      <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 text-white">
+        <h1 className="text-6xl font-bold text-center mb-4 tracking-tight">
+          Track Every Rep. Achieve Every Goal.
+        </h1>
+        <p className="text-xl text-center mb-8">
+          Your ultimate fitness companion. Stay on track, stay active with Tracktive.
+        </p>
+        <Link href="/dashboard" className="mb-10">
+          <Button size="lg">Get Started</Button>
+        </Link>
+        <div className="flex space-x-10">
+          <div className="flex flex-col items-center">
+            <div className="w-16 h-16 rounded-full mb-2">
+              <IconSearch className="w-16 h-16" /> 
+            </div>
+            <span>Browse Exercises</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-16 h-16 rounded-full mb-2">
+              <IconFilePlus className="w-16 h-16" />
+            </div>
+            <span>Create Routines</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-16 h-16 rounded-full mb-2">
+              <IconNotebook className="w-16 h-16" />
+            </div>
+            <span>Log Workouts</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
