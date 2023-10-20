3c541da7fe9ea11a859f81bd1e35928e67e06a4b
Green Gem <chris_waitt@me.com>
2023-10-20 17:13:45 +0100

    Expanded dashboard

--- app/(protected)/dashboard/page.js
+++ app/(protected)/dashboard/page.js
@@ -46,28 +46,44 @@ async function getWorkouts(userId) {
 
 
 function formatDuration(seconds) {
-	const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes}m ${remainingSeconds}s`;
+    const minutes = Math.floor(seconds / 60);
+    return `${minutes}m`;
 }
+
 
 export default async function DashboardPage() {
 	const session = await getServerSession(authOptions);
 	const workouts = await getWorkouts(session.user.id)
+	const totalWeightLifted = workouts.reduce((totalWeight, workout) => {
+		return totalWeight + workout.exercises.reduce((exerciseWeight, exercise) => {
+			return exerciseWeight + exercise.sets.reduce((setWeight, set) => setWeight + set.weight, 0);
+		}, 0);
+	}, 0);
+	const totalDuration = workouts.reduce((acc, workout) => acc + workout.duration, 0);
+	const averageDuration = workouts.length ? totalDuration / workouts.length : 0;
+	const formattedAverageDuration = formatDuration(averageDuration);
 
 	return (
 		<>
 			<PageHeading title="Dashboard" />
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 mb-5">
+				<Card><CardHeader className="px-5">Workouts Completed</CardHeader><CardBody className="text-5xl pt-0">{workouts.length}</CardBody></Card>
+				<Card><CardHeader className="px-5">Total Weight Lifted (kg)</CardHeader><CardBody className="text-5xl pt-0">{totalWeightLifted}</CardBody></Card>
+				<Card><CardHeader className="px-5">Average Workout Duration</CardHeader><CardBody className="text-5xl pt-0">{formattedAverageDuration}</CardBody></Card>
+				<Card><CardHeader className="px-5">TODO</CardHeader><CardBody className="text-3xl pt-0">TODO</CardBody></Card>
+			</div>
+
+			<Card className="mb-10"><CardHeader>Workouts Over Time</CardHeader><CardBody className="text-3xl pt-0">CHART</CardBody></Card>
+
+			<PageHeading title="History" />
 			<CardGrid>
 				{workouts.map((workout) => {
 					const totalWeightLifted = workout.exercises.reduce((acc, exercise) => {
 						return acc + exercise.sets.reduce((acc, set) => acc + set.weight, 0);
 					}, 0);
-					  
-
 					return (
 						<Card key={workout.id}>
-							<CardHeader className="flex gap-3">
+							<CardHeader className="flex gap-3 px-5">
 								<Image
 									alt="Barbell Icon"
 									height={40}
@@ -80,10 +96,10 @@ export default async function DashboardPage() {
 									<p className="text-small text-default-500">{formatDuration(workout.duration)} | {totalWeightLifted} KG</p>
 								</div>
 							</CardHeader>
-							<CardBody>
+							<CardBody className="py-0 pb-2">
 								<DashboardExerciseTable workoutLogExercises={workout.exercises} workoutName={workout.name} workoutDate={workout.createdAt} />
 							</CardBody>
-							<CardFooter>
+							<CardFooter className="px-5">
 								<DeleteButton id={workout.id} />
 							</CardFooter>
 						</Card>
