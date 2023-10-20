7916eaafc6a39725397ff9d442eaeed2f50d59d7
Green Gem <chris_waitt@me.com>
2023-10-20 18:06:58 +0100

    Added nav links

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,9 +5,9 @@ export default function Navbar() {
         <div className="p-5 bg-gray-900 block md:hidden">
             <ul className="flex space-x-2">
                 <li><Link href="/dashboard">Dashboard</Link></li>
-                <li><Link href="/dashboard">Workout</Link></li>
-                <li><Link href="/dashboard">Routines</Link></li>
-                <li><Link href="/dashboard">Exercises</Link></li>
+                <li><Link href="/workout">Workout</Link></li>
+                <li><Link href="/routines">Routines</Link></li>
+                <li><Link href="/exercises">Exercises</Link></li>
             </ul>
         </div>
     )
