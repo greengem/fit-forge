69b54e9d2467f70952f9db0b793d9da2e8253d5e
Green Gem <chris_waitt@me.com>
2023-10-21 14:23:52 +0100

    Missing style on Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <div className="px-5 py-3 dark:bg-custom-gray block md:hidden shadow-md">
-            <ul className="flex space-x-3">
+        <div className="px-5 py-3 bg-white dark:bg-custom-gray block md:hidden shadow-md">
+            <ul className="flex space-x-2">
                 <li><Link href="/dashboard">Dashboard</Link></li>
                 <li><Link href="/workout">Workout</Link></li>
                 <li><Link href="/routines">Routines</Link></li>
