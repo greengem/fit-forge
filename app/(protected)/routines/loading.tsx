a48c6782b5a9f41645c92b15c2d467522b1c2840
Green Gem <chris_waitt@me.com>
2023-10-21 16:49:53 +0100

    Simple Route Loading pages

---
+++ app/(protected)/routines/loading.tsx
@@ -0,0 +1,10 @@
+import {Spinner} from "@nextui-org/spinner";
+
+export default function Loading() {
+    return (
+        <div className="flex justify-center items-center h-screen w-full">
+            <Spinner label="Loading..." color="primary" />
+        </div>
+
+    )
+}
