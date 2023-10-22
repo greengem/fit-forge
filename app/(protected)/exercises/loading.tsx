b86b65ab5e980fc8e5e3d992dc297b5d041bf472
Green Gem <chris_waitt@me.com>
2023-10-22 10:35:57 +0100

    Improved Loading pages, may add skeletons later when UI is finished

--- app/(protected)/exercises/loading.tsx
+++ app/(protected)/exercises/loading.tsx
@@ -1,10 +1,13 @@
+import PageHeading from '@/components/PageHeading/PageHeading';
 import {Spinner} from "@nextui-org/spinner";
 
 export default function Loading() {
     return (
-        <div className="flex justify-center items-center h-screen w-full">
-            <Spinner label="Loading..." color="primary" />
-        </div>
-
+        <>
+            <PageHeading title="Exercises" />
+            <div className="flex justify-center items-center h-screen w-full">
+                <Spinner label="Loading..." color="primary" />
+            </div>
+        </>
     )
 }
