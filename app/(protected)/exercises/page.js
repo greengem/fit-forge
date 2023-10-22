4fc1f83d7b5607328453619a61b18b793f249cc4
Green Gem <chris_waitt@me.com>
2023-10-22 16:12:01 +0100

    Testing force Dynamic on Exercises

--- app/(protected)/exercises/page.js
+++ app/(protected)/exercises/page.js
@@ -2,6 +2,8 @@ import getExercises from '@/utils/getExercises';
 import PageHeading from '@/components/PageHeading/PageHeading';
 import ExerciseList from './ExerciseList';
 
+export const dynamic = 'force-static'
+
 export default async function ExercisesPage() {
   const exercises = await getExercises()
 
