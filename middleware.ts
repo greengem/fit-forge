aecaec5a3b4c038250a5fc5d932a6ce735b7f173
Green Gem <chris_waitt@me.com>
2023-10-22 10:35:36 +0100

    Middleware configured to protect correct pages

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,11 @@
 export { default } from "next-auth/middleware";
+
+export const config = {
+    matcher: [
+        "/dashboard", 
+        "/routines", 
+        "/workouts", 
+        "/profile", 
+        "/exercises", 
+    ],
+};
