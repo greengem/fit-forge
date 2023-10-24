23b74654f83be3e251c25a67f2fdbf6f70629518
Green Gem <chris_waitt@me.com>
2023-10-24 13:30:15 +0100

    Tidy up homepage

--- app/page.js
+++ app/page.js
@@ -4,35 +4,15 @@ import { IconSearch, IconFilePlus, IconNotebook } from "@tabler/icons-react"
 
 export default function HomePage() {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-teal-500 to-blue-600">
+    <div className="relative h-screen">
       <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 text-white">
         <h1 className="text-6xl font-bold text-center mb-4 tracking-tight">
-          Track Every Rep. Achieve Every Goal.
+          Track Every <span className="text-success">Rep</span>. Achieve Every <span className="text-success">Goal</span>.
         </h1>
         <p className="text-xl text-center mb-8">
           Your ultimate fitness companion. Stay on track, stay active with Tracktive.
         </p>
-        <Button as={Link} href="/dashboard" size="lg" className="mb-10">Get Started</Button>
-        <div className="flex space-x-10">
-          <div className="flex flex-col items-center">
-            <div className="w-16 h-16 rounded-full mb-2">
-              <IconSearch className="w-16 h-16" /> 
-            </div>
-            <span>Browse Exercises</span>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="w-16 h-16 rounded-full mb-2">
-              <IconFilePlus className="w-16 h-16" />
-            </div>
-            <span>Create Routines</span>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="w-16 h-16 rounded-full mb-2">
-              <IconNotebook className="w-16 h-16" />
-            </div>
-            <span>Log Workouts</span>
-          </div>
-        </div>
+        <Button color="success" as={Link} href="/dashboard" size="lg" className="mb-10">Get Started</Button>
       </div>
     </div>
   )
