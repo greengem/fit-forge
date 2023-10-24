6f605a92b0e09ae0b75e512c862c7dd386c4b333
Green Gem <chris_waitt@me.com>
2023-10-24 17:34:00 +0100

    Added Google and Email as auth providers

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,8 +1,9 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import type { NextAuthOptions } from "next-auth";
 import prisma from '@/db/prisma';
+import EmailProvider from "next-auth/providers/email";
 import GitHubProvider from "next-auth/providers/github";
-
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -29,5 +30,20 @@ session: async ({ session, token }) => {
             clientId: process.env.GITHUB_ID ?? "",
             clientSecret: process.env.GITHUB_SECRET ?? "",
         }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+          }),
+          EmailProvider({
+            server: {
+              host: process.env.EMAIL_SERVER_HOST,
+              port: process.env.EMAIL_SERVER_PORT,
+              auth: {
+                user: process.env.EMAIL_SERVER_USER,
+                pass: process.env.EMAIL_SERVER_PASSWORD,
+              },
+            },
+            from: process.env.EMAIL_FROM,
+          }),
     ],
 };
