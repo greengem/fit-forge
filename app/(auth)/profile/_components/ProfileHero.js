72739055a341eff4a17ca5f1819399be14e4e247
Green Gem <chris_waitt@me.com>
2023-10-24 17:34:38 +0100

    fix: google avatars throw 403 forbidden if i dont pass referrerPolicy: "no-referrer" as a prop to the avatar component

--- app/(auth)/profile/_components/ProfileHero.js
+++ app/(auth)/profile/_components/ProfileHero.js
@@ -3,7 +3,15 @@ import {Avatar, AvatarGroup, AvatarIcon} from "@nextui-org/avatar";
 export default function ProfileHero({session}){
     return(
         <div className="hero flex flex-col justify-center items-center pb-5 pt-10">
-            <Avatar color="success" isBordered src={session.user.image} className="w-20 h-20 text-large mb-5" />
+            <Avatar 
+                color="success" 
+                isBordered 
+                showFallback
+                name={session.user.name}
+                src={session.user.image} 
+                imgProps={{ referrerPolicy: "no-referrer" }}
+                className="w-20 h-20 text-large mb-5" 
+            />
             <h4 className="text-2xl mb-1">{session.user.name}</h4>
             <p className="text-xs text-gray-500">United Kingdom</p>
         </div>
