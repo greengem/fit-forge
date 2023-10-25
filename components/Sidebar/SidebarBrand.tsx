886fbb97ac7d6cfb2fc5025b989ffaab657af1b4
Green Gem <chris_waitt@me.com>
2023-10-25 11:13:40 +0100

    Added link to brand section

--- components/Sidebar/SidebarBrand.tsx
+++ components/Sidebar/SidebarBrand.tsx
@@ -1,10 +1,13 @@
 import { IconBarbell } from "@tabler/icons-react"
+import Link from "next/link"
 
 export default function SidebarBrand() {
     return (
         <div className="flex items-center justify-center flex-col pt-5 pb-2">
-            <IconBarbell stroke={1} size={64} className="dark:text-success" />
-            <p className="uppercase text-xs text-gray-500">Tracktive</p>
+            <Link href="/">
+                <IconBarbell stroke={1} size={64} className="dark:text-success" />
+                <p className="uppercase text-xs text-gray-500">Tracktive</p>
+            </Link>
         </div>
     )
 }
