bb3da75704be123a51f343ca92dc4256d54e1582
Green Gem <chris_waitt@me.com>
2023-10-25 18:21:53 +0100

    Landing page framer motion effects

--- app/page.js
+++ app/page.js
@@ -1,21 +1,44 @@
+"use client";
+import { motion } from 'framer-motion';
 import Link from "next/link"
 import { Button } from "@nextui-org/button"
 import { IconBarbell } from "@tabler/icons-react"
 
+const barbellSpin = {
+  hidden: { rotate: -90, opacity: 0 },
+  visible: { rotate: 0, opacity: 1, transition: { type: "spring", damping: 10 } }
+};
+
+const fadeInUp = {
+  hidden: { opacity: 0, y: 30 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } }
+};
+
 export default function HomePage() {
   return (
     <div className="relative h-screen">
-      <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 text-white">
-        <h1 className="text-6xl font-bold text-center mb-4 tracking-tight">
+      <motion.div className="relative z-10 flex flex-col items-center justify-center h-full px-4 text-white"
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.div variants={barbellSpin}>
+          <IconBarbell size={200} />
+        </motion.div>
+        <motion.h1 
+          className="text-6xl font-bold text-center mb-2 tracking-tight"
+          variants={fadeInUp}
+        >
           Track Every <span className="text-success">Rep</span>. Achieve Every <span className="text-success">Goal</span>.
-        </h1>
-        <p className="text-xl text-center mb-8">
+        </motion.h1>
+        <motion.p className="text-xl text-center mb-10" variants={fadeInUp}>
           Your ultimate fitness companion. Stay on track, stay active with Tracktive.
-        </p>
-        <Button color="success" as={Link} href="/dashboard" size="lg" className="mb-10 gap-unit-1">
-          <IconBarbell />Get Started 
-        </Button>
-      </div>
+        </motion.p>
+        <motion.div variants={fadeInUp}>
+          <Button color="success" as={Link} href="/dashboard" size="lg" className="mb-10 gap-unit-1">
+            <IconBarbell />Get Started 
+          </Button>
+        </motion.div>
+      </motion.div>
     </div>
-  )
+  );
 }
