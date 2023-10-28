95c3234ff05c45d0cae70cb54fc7a57d2c67536c
Green Gem <chris_waitt@me.com>
2023-10-28 10:54:39 +0100

    Update to Nextjs 14 - Moved themeColor and viewport from metadata to generateViewport. - NextAuth updated.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,20 @@ import {Providers} from "./providers";
 import type { Metadata } from 'next';
 import './globals.css';
 
+export function generateViewport() {
+  return {
+    themeColor: '#18181b',
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: 'no'
+  };
+}
+
 export const metadata: Metadata = {
   
   title: 'Tracktive: Log, Analyze, and Optimize Your Workouts',
   description: 'Take Control of Your Fitness Goals with Tracktive. The intuitive workout tracking web app designed to optimize your gym sessions and improve your results.',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
-  themeColor: '#18181b',
 }
 
 export default async function RootLayout({
