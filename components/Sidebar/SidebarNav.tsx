a6dfcea645a9e471d978e0579e9ced5a5049b0aa
Green Gem <chris_waitt@me.com>
2023-11-01 12:06:53 +0000

    Sidebar cleanup and beginning of V2 Sidebar

--- components/Sidebar/SidebarNav.tsx
+++ components/Sidebar/SidebarNav.tsx
@@ -3,7 +3,7 @@ import { usePathname } from 'next/navigation'
 import { signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import { IconDashboard, IconJumpRope, IconList, IconStretching, IconUser, IconActivity, IconLogout } from '@tabler/icons-react';
-import { Button } from '@nextui-org/button';
+import { Button, Divider } from '@nextui-org/react';
 
 export default function SidebarNav() {
     const pathname = usePathname();
@@ -15,48 +15,43 @@ export default function SidebarNav() {
                 label="Profile"
                 href="/profile"
                 active={pathname === "/profile"}
-                subtext="View your profile"
             />
             <NavItem
                 icon={<IconDashboard className="h-6 w-6" />}
                 label="Dashboard"
                 href="/dashboard"
                 active={pathname === "/dashboard"}
-                subtext="View your progress"
             />
             <NavItem
                 icon={<IconActivity className="h-6 w-6" />}
                 label="Activity"
                 href="/activity"
                 active={pathname === "/activity"}
-                subtext="View workout logs"
             />
+            <Divider />
             <NavItem
-                icon={<IconJumpRope className="h-6 w-6" />}
+                icon={<IconJumpRope className="h-7 w-7" />}
                 label="Start Workout"
                 href="/workout"
                 active={pathname.startsWith("/workout")}
-                subtext="Kick off a session"
             />
             <NavItem
-                icon={<IconList className="h-6 w-6" />}
-                label="Routines"
+                icon={<IconList className="h-7 w-7" />}
+                label="Manage Routines"
                 href="/routines"
                 active={pathname.startsWith("/routines")}
-                subtext="Manage your plans"
             />
             <NavItem
-                icon={<IconStretching className="h-6 w-6" />}
-                label="Exercises"
+                icon={<IconStretching className="h-7 w-7" />}
+                label="Browse Exercises"
                 href="/exercises"
                 active={pathname === "/exercises"}
-                subtext="View all exercises"
             />
+            <Divider />
             <NavItem
-                icon={<IconLogout className="h-6 w-6" />}
+                icon={<IconLogout className="h-7 w-7" />}
                 label="Sign Out"
                 active={false}
-                subtext="Logout from account"
                 onClick={() => signOut({ callbackUrl: '/' })}
             />
         </ul>
@@ -68,23 +63,21 @@ interface NavItemProps {
     label: string;
     href?: string;
     active: boolean;
-    subtext: string;
     onClick?: () => void;
 }
 
-function NavItem({ icon, label, href, active, subtext, onClick }: NavItemProps) {
+function NavItem({ icon, label, href, active, onClick }: NavItemProps) {
     const content = (
-        <div className="flex items-center space-x-2">
+        <div className={`flex items-center space-x-3 ${active && 'text-success'} text-foreground`}>
             {icon}
             <div>
                 <div>{label}</div>
-                <div className={`text-xs ${active ? 'text-gray-800' : 'text-gray-500'}`}>{subtext}</div>
             </div>
         </div>
     );
 
     return (
-        <li className={`px-2 py-1 rounded-lg ${active ? 'bg-success text-black' : 'hover:bg-default-200'}`}>
+        <li>
             {onClick ? (
                 <button className='text-left' onClick={onClick}>{content}</button>
             ) : (
