ffaf81d3d2d6e2c757a949b8471d9ecb6776f2d4
Green Gem <chris_waitt@me.com>
2023-11-14 16:39:45 +0000

    feat: Sidebar workout controls. allows easy pausing and display of workout times in the sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import SidebarNav from './SidebarNav';
 import SidebarBrand from './SidebarBrand';
 import { ThemeSwitcher } from '@/components/ThemeSwitcher/ThemeSwitcher';
+import SidebarWorkoutControls from './SidebarWorkoutControls';
 
 export default function Sidebar() {
     return (
@@ -8,9 +9,10 @@ export default function Sidebar() {
             <div>
                 <SidebarBrand />
             </div>
-            <div className='p-3'>
+            <div className='px-3 pt-3'>
                 <SidebarNav />
             </div>
+            <SidebarWorkoutControls />
 
             <div className='absolute bottom-0 left-0 right-0'>
                 <div className="py-5 flex flex-col items-center justify-center">
---
+++ components/Sidebar/SidebarWorkoutControls.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { usePathname } from 'next/navigation';
+import { format } from 'date-fns';
+import Link from 'next/link';
+import { useWorkoutControls } from '@/contexts/WorkoutControlsContext';
+import { Button, Divider } from '@nextui-org/react';
+import { IconPlayerPause, IconPlayerPlay } from '@tabler/icons-react';
+
+export default function SidebarWorkoutControls() {
+    const { isPaused, setIsPaused, workoutDuration, workoutStartTime, formatDuration, activeWorkoutRoutine } = useWorkoutControls();
+    const pathname = usePathname();
+    const workoutPath = `/workout/${activeWorkoutRoutine}`;
+    const formattedStartTime = format(new Date(workoutStartTime), "HH:mm");
+    const handlePauseToggle = () => {
+        setIsPaused(!isPaused);
+    };
+
+    return (
+        <>
+            {workoutStartTime !== null && (
+                <div className='px-3'>
+                    <Divider />
+                    <div className='px-3 py-3 rounded-lg'>
+                        <div className='text-center'>{isPaused ? 'Workout Paused' : 'Active Workout'}</div>
+                        <div className={`text-center text-3xl mb-2 tracking-tight ${isPaused ? 'text-warning' : ''}`}>{formatDuration(workoutDuration)}</div>
+                        <div className='flex justify-center gap-x-2 mb-2'>
+                            <Button color={isPaused ? 'success' : 'warning'} onClick={handlePauseToggle}>
+                                {isPaused ? <><IconPlayerPlay />Resume</> : <><IconPlayerPause />Pause</>}
+                            </Button>
+                        </div>
+                        {workoutPath !== pathname && (
+                            <div className='text-center text-sm text-success'>
+                                <Link href={workoutPath}>View Details</Link>
+                            </div>
+                        )}
+                        <div className='text-center text-xs text-default-500'>Start Time: {formattedStartTime}</div>
+                    </div>
+                </div>
+            )}            
+        </>
+    )
+}
