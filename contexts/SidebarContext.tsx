1b83868b1e8facb573f02fcd850bd554dca4bc4b
Green Gem <chris_waitt@me.com>
2023-11-15 14:49:24 +0000

    Updated Sidebar context to hold a state for slim mode, this will be an option where users can hide the sidebar text and have a lim sidebar with only icons

--- contexts/SidebarContext.tsx
+++ contexts/SidebarContext.tsx
@@ -2,12 +2,16 @@ import React, { useState, createContext, useContext, ReactNode } from 'react';
 
 interface SidebarContextType {
   isSidebarVisible: boolean;
+  isSlim: boolean;
   toggleSidebar: () => void;
+  toggleSlim: () => void;
 }
 
 const SidebarVisibilityContext = createContext<SidebarContextType>({
   isSidebarVisible: true,
+  isSlim: false,
   toggleSidebar: () => {},
+  toggleSlim: () => {},
 });
 
 interface SidebarProviderProps {
@@ -16,13 +20,18 @@ interface SidebarProviderProps {
 
 export const SidebarProvider: React.FC<SidebarProviderProps> = ({ children }) => {
   const [isSidebarVisible, setSidebarVisible] = useState(true);
+  const [isSlim, setSlim] = useState(false); // State for slim mode
 
   const toggleSidebar = () => {
     setSidebarVisible(!isSidebarVisible);
   };
 
+  const toggleSlim = () => {
+    setSlim(!isSlim);
+  };
+
   return (
-    <SidebarVisibilityContext.Provider value={{ isSidebarVisible, toggleSidebar }}>
+    <SidebarVisibilityContext.Provider value={{ isSidebarVisible, toggleSidebar, isSlim, toggleSlim }}>
       {children}
     </SidebarVisibilityContext.Provider>
   );
