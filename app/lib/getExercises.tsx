25e08f522ba4589b7fe962ccf469363ee8f37bf4
Green Gem <chris_waitt@me.com>
2023-11-15 14:41:36 +0000

    Cache Exerises, I added a revalidation tag for future use but this is unlikely to be need since the data is currently static

--- app/lib/getExercises.tsx
+++ app/lib/getExercises.tsx
@@ -1,26 +1,38 @@
 import prisma from '@/db/prisma';
 import { Exercise } from '@/types/ExerciseType';
+import { unstable_cache } from 'next/cache';
+
+const getCachedExercises = unstable_cache(
+    async () => {
+        console.log("Fetching exercises from database");
+        return await prisma.exercise.findMany({
+            select: {
+                id: true,
+                name: true,
+                aliases: true,
+                primary_muscles: true,
+                secondary_muscles: true,
+                force: true,
+                level: true,
+                mechanic: true,
+                equipment: true,
+                category: true,
+                instructions: true,
+                tips: true,
+                image: true,
+            },
+            orderBy: {
+                name: 'asc',
+            },
+        });
+    },
+    ['exerciseList'],
+    {
+        tags: ['exercises'],
+        revalidate: 30000,
+    }
+);
 
 export default async function getExercises(): Promise<Exercise[]> {
-    const exercises = await prisma.exercise.findMany({
-        select: {
-            id: true,
-            name: true,
-            aliases: true,
-            primary_muscles: true,
-            secondary_muscles: true,
-            force: true,
-            level: true,
-            mechanic: true,
-            equipment: true,
-            category: true,
-            instructions: true,
-            tips: true,
-            image: true,
-        },
-        orderBy: {
-            name: 'asc',
-        },
-    });
-    return exercises;
+    return await getCachedExercises();
 }
