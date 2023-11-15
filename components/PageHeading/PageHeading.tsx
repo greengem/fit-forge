dc75c691319a757ab94cebaf4b1b96d1c1d93d35
Green Gem <chris_waitt@me.com>
2023-11-15 14:47:43 +0000

    Fix heading styling

--- components/PageHeading/PageHeading.tsx
+++ components/PageHeading/PageHeading.tsx
@@ -3,5 +3,5 @@ type PageHeadingProps = {
 };
 
 export default function PageHeading({ title }: PageHeadingProps) {
-    return <h1 className="text-4xl mb-6 md:mt-8 font-semibold">{title}</h1>;
+    return <h1 className="text-4xl mb-6 mt-3 md:mt-8 font-semibold">{title}</h1>;
 }
