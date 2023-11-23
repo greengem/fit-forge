49770dbcbda2d3d7e7bd3e0ada87d3c37c6b8fd6
Green Gem <chris_waitt@me.com>
2023-11-23 12:17:10 +0000

    Updated query to only get what we need for exercises

--- app/lib/getExercises.tsx
+++ app/lib/getExercises.tsx
@@ -1,6 +1,17 @@
 import prisma from '@/db/prisma';
-import { Exercise } from '@/types/ExerciseType';
 import { unstable_cache } from 'next/cache';
+import { Muscle, EquipmentType, CategoryType } from '@prisma/client';
+
+type Exercise = {
+    id: string;
+    name: string;
+    primary_muscles: Muscle[];
+    secondary_muscles: Muscle[];
+    equipment: EquipmentType | null;
+    category: CategoryType;
+    image: string | null;
+};
+
 
 const getCachedExercises = unstable_cache(
     async () => {
@@ -9,16 +20,16 @@ const getCachedExercises = unstable_cache(
             select: {
                 id: true,
                 name: true,
-                aliases: true,
+                //aliases: true,
                 primary_muscles: true,
                 secondary_muscles: true,
-                force: true,
-                level: true,
-                mechanic: true,
+                //force: true,
+                //level: true,
+                //mechanic: true,
                 equipment: true,
                 category: true,
-                instructions: true,
-                tips: true,
+                //instructions: true,
+                //tips: true,
                 image: true,
             },
             orderBy: {
