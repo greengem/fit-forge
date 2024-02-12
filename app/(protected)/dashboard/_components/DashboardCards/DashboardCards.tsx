edd3abdc8d01e128ea1579786c8dfde784ddc621
Green Gem <chris_waitt@me.com>
2024-02-12 16:07:04 +0000

    Refactor DashboardCards component to use DashboardCardTemplate and Spinner

--- app/(protected)/dashboard/_components/DashboardCards/DashboardCards.tsx
+++ app/(protected)/dashboard/_components/DashboardCards/DashboardCards.tsx
@@ -1,16 +1,26 @@
 import { Suspense } from "react";
+import DashboardCardTemplate from "./DashboardCardTemplate";
 import DashboardCardAverageWorkoutDuration from "./DashboardCardAverageWorkoutDuration";
 import DashboardCardDailyStreak from "./DashboardCardDailyStreak";
 import DashboardCardWeeklyPbs from "./DashboardCardWeeklyPbs";
 import DashboardCardWeeklyWorkouts from "./DashboardCardWeeklyWorkouts";
+import {Spinner} from "@nextui-org/spinner";
 
 export default function DashboardCards() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 mb-5">
-            <Suspense fallback={<div>Loading...</div>}><DashboardCardWeeklyWorkouts /></Suspense>
-            <Suspense fallback={<div>Loading...</div>}><DashboardCardAverageWorkoutDuration /></Suspense>
-            <Suspense fallback={<div>Loading...</div>}><DashboardCardDailyStreak /></Suspense>
-            <Suspense fallback={<div>Loading...</div>}><DashboardCardWeeklyPbs /></Suspense>
+            <Suspense fallback={<DashboardCardTemplate title="Weekly Workouts"><Spinner color="primary" /></DashboardCardTemplate>}>
+                <DashboardCardWeeklyWorkouts />
+            </Suspense>
+            <Suspense fallback={<DashboardCardTemplate title="Weekly Workouts"><Spinner color="primary" /></DashboardCardTemplate>}>
+                <DashboardCardAverageWorkoutDuration />
+            </Suspense>
+            <Suspense fallback={<DashboardCardTemplate title="Weekly Workouts"><Spinner color="primary" /></DashboardCardTemplate>}>
+                <DashboardCardDailyStreak />
+            </Suspense>
+            <Suspense fallback={<DashboardCardTemplate title="Weekly Workouts"><Spinner color="primary" /></DashboardCardTemplate>}>
+                <DashboardCardWeeklyPbs />
+            </Suspense>
         </div>
     )
 }
