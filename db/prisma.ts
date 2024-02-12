0199e58af9bd2bdcc8c5549f150c425635226f36
Green Gem <chris_waitt@me.com>
2024-02-12 13:05:36 +0000

    Add Prisma Accelerate extension and update Prisma client instantiation

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
+// Factory function to create a PrismaClient instance extended with Accelerate
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  // Create a new PrismaClient instance
+  const prismaClient = new PrismaClient();
+
+  // Extend the PrismaClient instance with Prisma Accelerate
+  const prismaWithAccelerate = prismaClient.$extends(withAccelerate());
+
+  return prismaWithAccelerate;
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
@@ -10,8 +18,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined
 }
 
+// Use the extended Prisma client with Accelerate or create a new one if it doesn't exist
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 export default prisma
 
+// Reassign the Prisma client in global scope in non-production environments
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
