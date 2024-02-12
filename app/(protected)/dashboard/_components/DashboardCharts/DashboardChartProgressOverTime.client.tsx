57dfca5d77fbf3ca0119950231fd9fe665a40b4a
Green Gem <chris_waitt@me.com>
2024-02-12 10:19:01 +0000

    Refactor DashboardChartProgressOverTime.client.tsx

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardChartProgressOverTime.client.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardChartProgressOverTime.client.tsx
@@ -1,17 +1,29 @@
 'use client'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, XAxis, Tooltip, ResponsiveContainer, TooltipProps } from 'recharts';
 
 type WorkoutData = {
   date: Date;
   totalWeight: number;
 };
 
+function CustomTooltip({ active, payload, label }: TooltipProps<number, string>) {
+  if (active && payload && payload.length) {
+    return (
+      <div className="bg-zinc-800 text-white px-4 py-2 rounded-xl shadow-xl text-xs">
+        <p className='font-semibold'>Weight: <span className='text-primary'>{payload[0].value}</span> Kg</p>
+        <p>Week: {label}</p>
+      </div>
+    );
+  }
+}
+
 export default function DashboardChartProgressOverTimeClient({ data } : { data: WorkoutData[] }) {
     return (
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
           <Area type="monotone" dataKey="totalWeight" stroke="#A6FF00" fill="#A6FF00" />
-          <Tooltip />
+          <XAxis dataKey="week" tick={{ fontSize: '12px' }}  />
+          <Tooltip content={<CustomTooltip />} />
         </AreaChart>
       </ResponsiveContainer>
     );
