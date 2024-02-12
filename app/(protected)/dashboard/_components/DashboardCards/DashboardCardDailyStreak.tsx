83f0ec6d0d97cb7c61328a903456a9163f0c25d2
Green Gem <chris_waitt@me.com>
2024-02-12 09:11:39 +0000

    Add DashboardCardDailyStreak component

---
+++ app/(protected)/dashboard/_components/DashboardCards/DashboardCardDailyStreak.tsx
@@ -0,0 +1,40 @@
+// DashboardCardDailyStreak.tsx
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/authOptions"
+import prisma from "@/db/prisma";
+import { Card, CardHeader, CardBody } from "@nextui-org/card";
+import { differenceInCalendarDays } from 'date-fns';
+
+export default async function DashboardCardDailyStreak() {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id;
+
+    const workouts = await prisma.workoutLog.findMany({
+        where: {
+            userId: userId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+        select: {
+            createdAt: true,
+        },
+    });
+
+    let streak = 0;
+    for (let i = 0; i < workouts.length - 1; i++) {
+        const diff = differenceInCalendarDays(workouts[i].createdAt, workouts[i + 1].createdAt);
+        if (diff === 1) {
+            streak++;
+        } else if (diff > 1) {
+            break;
+        }
+    }
+
+    return (
+        <Card shadow="none" className="shadow-md">
+            <CardHeader className="px-5">Daily Streak</CardHeader>
+            <CardBody className="text-5xl pt-0 text-primary gap-y-3">{streak}</CardBody>
+        </Card>
+    );
+}
