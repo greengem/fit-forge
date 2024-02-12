badfbf2a595c4c0937ed78e8ef21e82074bf3bc6
Green Gem <chris_waitt@me.com>
2024-02-12 09:09:32 +0000

    Add DashboardCardAverageWorkoutDuration component

---
+++ app/(protected)/dashboard/_components/DashboardCards/DashboardCardAverageWorkoutDuration.tsx
@@ -0,0 +1,32 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/authOptions"
+import prisma from "@/db/prisma";
+import { Card, CardHeader, CardBody } from "@nextui-org/card";
+import { subDays } from 'date-fns';
+
+export default async function DashboardCardAverageWorkoutDuration() {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id;
+    const thirtyDaysAgo = subDays(new Date(), 30);
+    const workouts = await prisma.workoutLog.findMany({
+        where: {
+            userId: userId,
+            createdAt: {
+                gte: thirtyDaysAgo,
+            },
+        },
+        select: {
+            duration: true,
+        },
+    });
+
+    const totalDuration = workouts.reduce((total, workout) => total + workout.duration / 60, 0); // Convert seconds to minutes
+    const averageDuration = workouts.length > 0 ? Math.round(totalDuration / workouts.length) : 0;
+
+    return (
+        <Card shadow="none" className="shadow-md">
+            <CardHeader className="px-5">Average Workout Duration</CardHeader>
+            <CardBody className="text-5xl pt-0 text-primary gap-y-3">{averageDuration}m</CardBody>
+        </Card>
+    )
+}
