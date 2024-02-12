0873927319232ce4c83ec01076597f02940439ac
Green Gem <chris_waitt@me.com>
2024-02-12 12:12:11 +0000

    Add SpeedInsights component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export default async function RootLayout({
           <Toaster />
           {children}
         </Providers>
+        <SpeedInsights/>
         <Analytics />
       </body>
     </html>
