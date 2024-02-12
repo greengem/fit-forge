e60c29bb4da99c2e25c5257a66cccdb603435ec7
Green Gem <chris_waitt@me.com>
2024-02-12 09:15:14 +0000

    Remove unused imports and components from DashboardPage

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -2,13 +2,8 @@ import { Suspense } from "react";
 import PageHeading from '@/components/PageHeading/PageHeading';
 import DashboardGreeting from "./_components/DashBoardGreeting";
 import DashboardLinks from "./_components/DashboardLinks";
-import DashboardChartWorkouts from "./_components/DashboardChartWorkouts";
 import DashboardRecentActivity from "./_components/DashboardRecentActivity";
 import DashboardCards from "./_components/DashboardCards/DashboardCards";
-import DashboardChartProgressOverTime from "./_components/DashboardCharts/DashboardChartProgressOverTime";
-import DashboardChartWorkoutFrequency from "./_components/DashboardCharts/DashboardChartWorkoutFrequency";
-import DashboardChartExerciseCategoryDistribution from "./_components/DashboardCharts/DashboardChartExerciseCategoryDistribution";
-import DashboardChartVolumeLoad from "./_components/DashboardCharts/DashboardChartVolumeLoad";
 import DashboardCharts from "./_components/DashboardCharts/DashboardCharts";
 
 export default async function DashboardPage() {
