664dec8543444aeb8d213235e232bb6e97d20532
Green Gem <chris_waitt@me.com>
2024-02-12 10:21:24 +0000

    Add DashboardCardWeeklyPbs component

---
+++ app/(protected)/dashboard/_components/DashboardCards/DashboardCardWeeklyPbs.tsx
@@ -0,0 +1,11 @@
+// DashboardCardWeeklyPbs.tsx
+import { Card, CardHeader, CardBody } from "@nextui-org/card";
+
+export default function DashboardCardWeeklyPbs() {
+    return (
+        <Card shadow="none" className="shadow-md">
+            <CardHeader className="px-5">Weekly Personal Bests</CardHeader>
+            <CardBody className="text-xl pt-0 text-primary gap-y-3">Coming Soon</CardBody>
+        </Card>
+    );
+}
