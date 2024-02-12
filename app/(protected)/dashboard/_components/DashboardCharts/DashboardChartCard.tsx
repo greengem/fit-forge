3be694169d247b25142546d55150adfb1aa7f749
Green Gem <chris_waitt@me.com>
2024-02-12 11:09:26 +0000

    Update DashboardChartCard component layout

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardChartCard.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardChartCard.tsx
@@ -6,7 +6,7 @@ export default function DashboardChartCard({ children, title, colSpan }: { child
     return (
         <Card shadow="none" className={clsx('shadow-md h-72', colSpan)}>
             <CardHeader className="p-3 tracking-tight gap-5 items-center">
-                <p className='shrink-0 w-2/3'>{title}</p>
+                <p className='shrink-0 w-1/2 md:w-2/3'>{title}</p>
                 <Slider
                     size="md"
                     isDisabled
@@ -16,7 +16,7 @@ export default function DashboardChartCard({ children, title, colSpan }: { child
                     defaultValue={40}
                     maxValue={50}
                     minValue={0}
-                    className="grow w-1/3"
+                    className="grow w-1/2 md:w-1/3"
                     marks={[
                         {
                             value: 0,
