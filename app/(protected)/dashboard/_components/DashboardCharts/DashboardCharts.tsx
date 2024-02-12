1539536911647535dcb700ebcf4a71c2cbcd75a1
Green Gem <chris_waitt@me.com>
2024-02-12 10:18:40 +0000

    Update DashboardCharts layout

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardCharts.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardCharts.tsx
@@ -6,10 +6,10 @@ import DashboardChartWorkoutFrequency from "./DashboardChartWorkoutFrequency";
 
 export default function DashboardCharts(){
     return (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 mb-5">
+        <div className="grid grid-cols-2 xl:grid-cols-4 gap-5 mb-5">
+            <Suspense fallback={<div>Loading Chart...</div>}><DashboardChartWorkoutFrequency /></Suspense>
             <Suspense fallback={<div>Loading Chart...</div>}><DashboardChartProgressOverTime /></Suspense>
             <Suspense fallback={<div>Loading Chart...</div>}><DashboardChartVolumeLoad /></Suspense>
-            <Suspense fallback={<div>Loading Chart...</div>}><DashboardChartWorkoutFrequency /></Suspense>
             <Suspense fallback={<div>Loading Chart...</div>}><DashboardChartExerciseCategoryDistribution /></Suspense>
         </div>
     )
