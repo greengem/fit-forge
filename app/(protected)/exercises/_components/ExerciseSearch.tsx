e5216e8e43788d0576267e8e7b3c72e2463d1010
Green Gem <chris_waitt@me.com>
2024-02-14 13:19:53 +0000

    Add ExerciseSearch component for exercise search functionality

---
+++ app/(protected)/exercises/_components/ExerciseSearch.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import {Input} from "@nextui-org/input";
+
+export default function ExerciseSearch() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    const handleSearch = useDebouncedCallback((term) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
+        if (term) {
+            params.set('search', term);
+        } else {
+            params.delete('search');
+        }
+        replace(`${pathname}?${params.toString()}`, { scroll: false });
+    }, 300);
+
+    return (
+        <Input
+            placeholder='Search exercises...'
+            label='Search'
+            className='grow'
+            size='sm'
+            onChange={(e) => {
+                handleSearch(e.target.value);
+            }}
+            defaultValue={searchParams.get('search')?.toString()}
+        />
+    )
+}
