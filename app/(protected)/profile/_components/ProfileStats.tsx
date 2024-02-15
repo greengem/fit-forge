374436ad6fd14f08404f133b1c41bdde4eb0519f
Green Gem <chris_waitt@me.com>
2024-02-15 10:39:23 +0000

    Refactor ProfileStats component to use UserMeasurements interface

--- app/(protected)/profile/_components/ProfileStats.tsx
+++ app/(protected)/profile/_components/ProfileStats.tsx
@@ -1,36 +1,36 @@
-import { ExpandedProfile } from "@/types/ProfileType"
-
-interface ProfileStatsProps {
-    expandedProfile: ExpandedProfile;
+interface UserMeasurements {
+    weight?: number | null;
+    height?: number | null;
+    age?: number | null;
 }
 
-const ProfileStats: React.FC<ProfileStatsProps> = ({ expandedProfile }) => {
+export default function ProfileStats({ userMeasurements }: { userMeasurements: UserMeasurements }) {
     return (
         <div className="grid grid-flow-col lg:px-40 mb-20">
-            {expandedProfile.weight && (
+            {userMeasurements.weight && (
                 <div className="text-center">
                     <div className="mb-1 space-x-1">
-                        <span className="text-5xl">{expandedProfile.weight}</span>
+                        <span className="text-5xl">{userMeasurements.weight}</span>
                         <span className="text-gray-500">kg</span>
                     </div>
                     <div className="text-sm text-gray-500">Weight</div>
                 </div>
             )}
 
-            {expandedProfile.height && (
+            {userMeasurements.height && (
                 <div className="text-center">
                     <div className="mb-1 space-x-1">
-                        <span className="text-5xl">{expandedProfile.height}</span>
+                        <span className="text-5xl">{userMeasurements.height}</span>
                         <span className="text-gray-500">cm</span>
                     </div>
                     <div className="text-sm text-gray-500">Height</div>
                 </div>
             )}
 
-            {expandedProfile.age && (
+            {userMeasurements.age && (
                 <div className="text-center">
                     <div className="mb-1 space-x-1">
-                        <span className="text-5xl">{expandedProfile.age}</span>
+                        <span className="text-5xl">{userMeasurements.age}</span>
                         <span className="text-gray-500">y.o</span>
                     </div>
                     <div className="text-sm text-gray-500">Age</div>
@@ -39,5 +39,3 @@ const ProfileStats: React.FC<ProfileStatsProps> = ({ expandedProfile }) => {
         </div>
     )
 }
-
-export default ProfileStats;
