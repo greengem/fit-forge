76afecae804bdee21e27ba2218eb0602e6191598
Green Gem <chris_waitt@me.com>
2024-02-15 10:38:48 +0000

    Refactor ProfileHero component to use destructured props

--- app/(protected)/profile/_components/ProfileHero.tsx
+++ app/(protected)/profile/_components/ProfileHero.tsx
@@ -1,26 +1,19 @@
-import { Session } from "next-auth";
 import { Avatar } from "@nextui-org/avatar";
 
-interface ProfileHeroProps {
-    session: Session;
-}
-
-const ProfileHero: React.FC<ProfileHeroProps> = ({ session }) => {
+export default function ProfileHero({ userImage, username } : { userImage: string | undefined, username: string | undefined }) {
     return(
         <div className="hero flex flex-col justify-center items-center pb-5 pt-10">
             <Avatar 
                 color="primary" 
                 isBordered 
                 showFallback
-                name={session.user.name || "User"}
-                src={session.user.image} 
+                name={username}
+                src={userImage} 
                 imgProps={{ referrerPolicy: "no-referrer" }}
                 className="w-20 h-20 text-large mb-5" 
             />
-            <h4 className="text-2xl mb-1">{session.user.name || 'User'}</h4>
+            <h4 className="text-2xl mb-1">{username || "User"}</h4>
             <p className="text-xs text-gray-500">United Kingdom</p>
         </div>
     )
 }
-
-export default ProfileHero;
