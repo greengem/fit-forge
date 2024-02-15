6d90d45b8919c988c1477b5a2fd9ed37a5228774
Green Gem <chris_waitt@me.com>
2024-02-15 10:38:40 +0000

    Refactor ProfileActions component to use Clerk's SignOutButton

--- app/(protected)/profile/_components/ProfileActions.tsx
+++ app/(protected)/profile/_components/ProfileActions.tsx
@@ -1,35 +1,11 @@
 "use client";
-import { useRouter } from 'next/navigation'
-import toast from 'react-hot-toast';
-import { signOut } from "next-auth/react";
+import { SignOutButton } from "@clerk/nextjs";
 import { Button } from "@nextui-org/button";
 
-const handleDeleteAccount = async () => {
-  if (window.confirm("Are you sure you want to delete your account? This can't be undone.")) {
-    try {
-      const res = await fetch(`/api/users/`, {
-        method: 'DELETE',
-      });
-      if (res.ok) {
-        toast.success("Account and data deleted successfully");
-        signOut({ callbackUrl: '/' });
-      } else {
-        toast.error("An error occurred while trying to delete your account.");
-      }
-    } catch (error) {
-      toast.error("A network error occurred, please try again.");
-    }
-  }
-};
-
-
 export default function ProfileActions() {
-  const router = useRouter();
-  
   return (
-    <div className='flex gap-x-3'>
-      <Button onClick={() => signOut({ callbackUrl: '/'})}>Sign out</Button>
-      <Button color="danger" onPress={() => handleDeleteAccount()}>Delete my account and all my data</Button>
-    </div>
+    <SignOutButton>
+      <Button>Sign out</Button>
+    </SignOutButton>
   )
 }
