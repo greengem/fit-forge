8d25a4d9e1132a2fe4ee2beb8cd9bbf57b176eb1
Green Gem <chris_waitt@me.com>
2024-02-16 13:34:39 +0000

    Refactor DashboardChartCard component

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardChartCard.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardChartCard.tsx
@@ -1,52 +1,16 @@
-'use client'
 import clsx from 'clsx';
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
-import { Slider } from "@nextui-org/slider";
+import DashboardChartDateRange from './DashboardChartDateRange';
 
-export default function DashboardChartCard({ children, title, colSpan }: { children: React.ReactNode, title: string, colSpan?: string }) {
+export default function DashboardChartCard({ children, title, colSpan, chartId }: { children: React.ReactNode, title: string, colSpan?: string, chartId: number }) {
     return (
         <Card shadow="none" className={clsx('shadow-md h-72', colSpan)}>
-            <CardHeader className="p-3 tracking-tight gap-5 items-center">
-                <p className='shrink-0 w-1/2 md:w-2/3'>{title}</p>
-                <Slider
-                    size="md"
-                    isDisabled
-                    step={10}
-                    color="primary"
-                    showSteps={true}
-                    defaultValue={40}
-                    maxValue={50}
-                    minValue={0}
-                    className="grow w-1/2 md:w-1/3"
-                    marks={[
-                        {
-                            value: 0,
-                            label: "1D",
-                        },
-                        {
-                            value: 10,
-                            label: "3D",
-                        },
-                        {
-                            value: 20,
-                            label: "1W",
-                        },
-                        {
-                            value: 30,
-                            label: "1M",
-                        },
-                        {
-                            value: 40,
-                            label: "3M",
-                        },
-                        {
-                            value: 50,
-                            label: "1Y",
-                        },
-                    ]}
-                />
+            <CardHeader className="p-3 tracking-tight gap-5 items-start justify-between">
+                <p className='shrink-0 w-1/2'>{title}</p>
+                <DashboardChartDateRange chartId={chartId} />
             </CardHeader>
-            <CardBody className="p-3 text-black">{children}</CardBody>
+            <CardBody className="p-3 pb-0 text-black">{children}</CardBody>
         </Card>
     )
 }
+ 
