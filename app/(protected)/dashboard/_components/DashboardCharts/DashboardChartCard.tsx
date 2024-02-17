2adbce50fd23fd2b68640b1bf40dda574c45eba1
Green Gem <chris_waitt@me.com>
2024-02-17 10:20:21 +0000

    Add icon to DashboardChartCard

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardChartCard.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardChartCard.tsx
@@ -1,12 +1,13 @@
 import clsx from 'clsx';
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import DashboardChartDateRange from './DashboardChartDateRange';
+import React from 'react';
 
-export default function DashboardChartCard({ children, title, colSpan, chartId }: { children: React.ReactNode, title: string, colSpan?: string, chartId: number }) {
+export default function DashboardChartCard({ children, title, colSpan, chartId, icon }: { children: React.ReactNode, title: string, colSpan?: string, chartId: number, icon: React.ReactNode }) {
     return (
         <Card shadow="none" className={clsx('shadow-md h-72', colSpan)}>
             <CardHeader className="p-3 tracking-tight gap-5 items-start justify-between">
-                <p className='shrink-0 w-1/2'>{title}</p>
+                <p className='shrink-0 w-1/2 flex items-center gap-1'><span className='text-primary'>{icon}</span> {title}</p>
                 <DashboardChartDateRange chartId={chartId} />
             </CardHeader>
             <CardBody className="p-3 pb-0 text-black">{children}</CardBody>
