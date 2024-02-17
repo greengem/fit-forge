be109d8e1365f8bb72957dc0104074f5ec91f6ef
Green Gem <chris_waitt@me.com>
2024-02-17 19:33:18 +0000

    Refactor ExerciseAddToRoutineButton component

--- app/(protected)/exercises/_components/ExerciseAddToRoutineButton.tsx
+++ app/(protected)/exercises/_components/ExerciseAddToRoutineButton.tsx
@@ -1,16 +1,60 @@
 'use client'
-import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/popover";
+import { Exercise } from "@prisma/client"
+import { User } from "@nextui-org/user";
+import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/popover"
 import { Button } from "@nextui-org/button"
-import { IconList, IconPlus } from "@tabler/icons-react"
+import { IconPlus } from "@tabler/icons-react"
+import { Input } from "@nextui-org/input";
+import { Divider } from "@nextui-org/divider";
 
-export default function ExerciseAddToRoutineButton({ exerciseId } : { exerciseId: string }) {
+
+interface UserRoutine {
+    id: string;
+    name: string;
+    exerciseCount: number;
+}
+
+type ExerciseAddToRoutineButtonProps = {
+    exercise: Exercise;
+    userRoutines: UserRoutine[];
+}
+
+export default function ExerciseAddToRoutineButton({ exercise, userRoutines } : ExerciseAddToRoutineButtonProps) {
     return (
-        <Popover placement="left">
+        <Popover placement="left" showArrow={true}>
             <PopoverTrigger><Button isIconOnly isDisabled><IconPlus size={20} /></Button></PopoverTrigger>
             <PopoverContent>
-                <div className="px-1 py-2">
-                    <div className="flex items-center gap-1 font-bold mb-3"><IconList size={18} />Add to Routine</div>
-                    <div>This is the popover content</div>
+                <div className="px-2 pt-2 pb-1 min-w-64">
+                    <h4 className="mb-5 text-lg">Add <span className="font-semibold">{exercise.name}</span> to a Routine</h4>
+
+                    <h5 className="font-semibold mb-2">New Routine</h5>
+
+                    <div className="flex items-center gap-3 justify-between">
+                        <form>
+                            <Input
+                                placeholder="My awesome workout plan"
+                                labelPlacement="outside"
+                            >
+                            </Input>
+                        </form>
+                        <div className="shrink-0"><Button size="md" isIconOnly><IconPlus size={18} /></Button></div>
+                    </div>
+
+                    <Divider className="my-2" />
+
+                    <h5 className="font-semibold mb-2">Existing Routines</h5>
+
+                    <ul>
+                        {userRoutines.map((routine) => (
+                            <li key={routine.id} className="space-y-2 flex gap-x-2 justify-between items-center">
+                                <User   
+                                    name={routine.name}
+                                    description={`${routine.exerciseCount} Exercises`}
+                                />
+                                <div><Button size="md" variant="flat" isIconOnly><IconPlus size={18} /></Button></div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </PopoverContent>
         </Popover>
