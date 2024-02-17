ae12794e2a8878fe221eb62f3d99921a6455789d
Green Gem <chris_waitt@me.com>
2024-02-17 10:20:10 +0000

    Refactor DashboardPage component to accept searchParams

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -6,16 +6,35 @@ import DashboardRecentActivity from "./_components/DashboardRecentActivity";
 import DashboardCards from "./_components/DashboardCards/DashboardCards";
 import DashboardCharts from "./_components/DashboardCharts/DashboardCharts";
 
-export default async function DashboardPage() {
+export default function DashboardPage({ 
+	searchParams 
+} : {
+	searchParams?: {
+		chart1?: string,
+		chart2?: string,
+		chart3?: string,
+		chart4?: string,
+	  };
+}) {
+	const chart1DateRange = searchParams?.chart1 || '3M';
+	const chart2DateRange = searchParams?.chart2 || '3M';
+	const chart3DateRange = searchParams?.chart3 || '3M';
+	const chart4DateRange = searchParams?.chart4 || '3M';
+
 	return (
 		<>
 			<PageHeading title="Dashboard" />
-			<Suspense fallback={<div>Loading Greeting...</div>}>
+			{/* <Suspense fallback={<div>Loading Greeting...</div>}>
 				<DashboardGreeting />
-			</Suspense>
+			</Suspense> */}
 			<DashboardCards />
-			<DashboardLinks />
-			<DashboardCharts />
+			{/* <DashboardLinks /> */}
+			<DashboardCharts 
+				chart1DateRange={chart1DateRange} 
+				chart2DateRange={chart2DateRange} 
+				chart3DateRange={chart3DateRange} 
+				chart4DateRange={chart4DateRange} 
+			/>
 			<Suspense fallback={<div>Loading Recent Activity...</div>}>
 				<DashboardRecentActivity />
 			</Suspense>
