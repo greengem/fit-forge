0f98107e5730a17a7703d918af72ebe32b3bffcf
Green Gem <chris_waitt@me.com>
2024-02-17 14:48:52 +0000

    Fix truncation issue in DashboardChartCard

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardChartCard.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardChartCard.tsx
@@ -7,7 +7,7 @@ export default function DashboardChartCard({ children, title, colSpan, chartId, 
     return (
         <Card shadow="none" className={clsx('shadow-md h-72', colSpan)}>
             <CardHeader className="p-3 tracking-tight gap-5 items-start justify-between">
-                <p className='shrink-0 w-1/2 flex items-center gap-1'><span className='text-primary'>{icon}</span> {title}</p>
+                <p className='shrink-0 w-1/2 flex items-center gap-1 truncate'><span className='text-primary'>{icon}</span> {title}</p>
                 <DashboardChartDateRange chartId={chartId} />
             </CardHeader>
             <CardBody className="p-3 pb-0">{children}</CardBody>
