6f241bc947318d3e181a4cba55c19762e76a4fcc
Green Gem <chris_waitt@me.com>
2024-02-17 12:04:56 +0000

    Add limit to recent activity query

--- app/(protected)/dashboard/_components/DashboardRecentActivity.tsx
+++ app/(protected)/dashboard/_components/DashboardRecentActivity.tsx
@@ -22,6 +22,7 @@ export default async function DashboardRecentActivity() {
         where: {
             userId: userId,
         },
+        take: 4,
         orderBy: {
             createdAt: 'desc',
         },
