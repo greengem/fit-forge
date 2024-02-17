90a6beaf6f471bb6129c46948e87986f28974e9d
Green Gem <chris_waitt@me.com>
2024-02-17 10:20:59 +0000

    Update period label in DashboardChartProgressOverTime.client.tsx

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardChartProgressOverTime.client.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardChartProgressOverTime.client.tsx
@@ -2,7 +2,7 @@
 import { AreaChart, Area, XAxis, Tooltip, ResponsiveContainer, TooltipProps } from 'recharts';
 
 type WorkoutData = {
-  date: Date;
+  period: string;
   totalWeight: number;
 };
 
@@ -11,20 +11,22 @@ function CustomTooltip({ active, payload, label }: TooltipProps<number, string>)
     return (
       <div className="bg-zinc-800 text-white px-4 py-2 rounded-xl shadow-xl text-xs">
         <p className='font-semibold'>Weight: <span className='text-primary'>{payload[0].value}</span> Kg</p>
-        <p>Week: {label}</p>
+        <p>Period: {label}</p>
       </div>
     );
   }
+
+  return null;
 }
 
 export default function DashboardChartProgressOverTimeClient({ data } : { data: WorkoutData[] }) {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }} aria-label="Progress Over Time Area Chart">
-          <Area type="monotone" dataKey="totalWeight" stroke="#A6FF00" fill="#A6FF00" aria-label="Total Weight Area" />
-          <XAxis dataKey="week" tick={{ fontSize: '12px' }}  />
-          <Tooltip content={<CustomTooltip />} />
-        </AreaChart>
-      </ResponsiveContainer>
-    );
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <AreaChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }} aria-label="Progress Over Time Area Chart">
+        <Area type="monotone" dataKey="totalWeight" stroke="#A6FF00" fill="#A6FF00" aria-label="Total Weight Area" />
+        <XAxis dataKey="period" tick={{ fontSize: '10px' }}  />
+        <Tooltip content={<CustomTooltip />} />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 }
