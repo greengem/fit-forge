3b940c409bd13043249321a7e365a362f04d9db2
Green Gem <chris_waitt@me.com>
2024-02-17 10:21:29 +0000

    Refactor DashboardChartProgressOverTime component

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardChartProgressOverTime.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardChartProgressOverTime.tsx
@@ -1,57 +1,86 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/lib/authOptions"
-import prisma from "@/db/prisma";
-import DashboardChartCard from './DashboardChartCard';
+import { auth } from "@clerk/nextjs";
+import prisma from "@/prisma/prisma";
+import { format } from 'date-fns';
 import DashboardChartProgressOverTimeClient from './DashboardChartProgressOverTime.client';
+import { calculateIntervals, getIntervalStartAndEndDates } from "./utils/dateUtils";
 
 type WorkoutData = {
-    date: Date;
+    period: string;
     totalWeight: number;
 };
 
-export default async function DashboardChartProgressOverTime() {
-    const session = await getServerSession(authOptions);
-    const userId = session?.user?.id;
-    let totalWeightLiftedOverTime: WorkoutData[] = await prisma.workoutLog.findMany({
-        where: {
-          userId: userId,
-        },
+export default async function DashboardChartProgressOverTime({ dateRange = '3M' }: { dateRange?: string }) {
+  const { userId }: { userId: string | null } = auth();
+
+  if (!userId) {
+    throw new Error('You must be signed in to view this page.');
+  }
+
+  const startDate = await prisma.workoutLog.findFirst({
+    where: { userId: userId },
+    orderBy: { date: 'asc' },
+    select: { date: true },
+  });
+  
+  const intervals = calculateIntervals(dateRange);
+
+  let workoutLogs = await prisma.workoutLog.findMany({
+    where: {
+      userId: userId,
+      date: {
+        gte: startDate?.date || new Date(),
+        lte: new Date(),
+      },
+    },
+    select: {
+      date: true,
+      exercises: {
         select: {
-          date: true,
-          exercises: {
+          sets: {
             select: {
-              sets: {
-                select: {
-                  weight: true,
-                },
-              },
+              weight: true,
             },
           },
         },
-        orderBy: {
-          date: 'asc',
-        },
-      }).then((workoutLogs) =>
-        workoutLogs.map((log) => ({
-          date: log.date,
-          totalWeight: log.exercises.reduce(
-            (total, exercise) => total + exercise.sets.reduce((setTotal, set) => setTotal + (set.weight || 0), 0),
-            0
-          ),
-        }))
-      );
+      },
+    },
+    orderBy: {
+      date: 'asc',
+    },
+  });
 
-    let accumulatedWeight = 0;
-    totalWeightLiftedOverTime = totalWeightLiftedOverTime.map(item => {
-        accumulatedWeight += item.totalWeight;
-        return { ...item, totalWeight: accumulatedWeight };
+  let cumulativeTotalWeight = 0;
+  
+  let cumulativeWeights = workoutLogs.map(log => {
+    const totalWeightForLog = log.exercises.reduce(
+      (total, exercise) => total + exercise.sets.reduce((setTotal, set) => setTotal + (set.weight || 0), 0),
+      0
+    );
+    cumulativeTotalWeight += totalWeightForLog;
+    return {
+      period: format(log.date, 'yyyy-MM-dd'),
+      totalWeight: cumulativeTotalWeight
+    };
+  });
+
+  const adjustedData = intervals.map(interval => {
+    const { startOfInterval, endOfInterval } = getIntervalStartAndEndDates(interval, dateRange);
+    const period = format(startOfInterval, 'dd-MM-yyyy');
+
+    let cumulativeWeightsUpToInterval = cumulativeWeights.filter(item => {
+      const itemDate = new Date(item.period);
+      return itemDate <= endOfInterval;
     });
 
-    //console.log(totalWeightLiftedOverTime);
+    let lastCumulativeWeight = cumulativeWeightsUpToInterval.length > 0
+      ? cumulativeWeightsUpToInterval[cumulativeWeightsUpToInterval.length - 1].totalWeight
+      : 0;
 
-    return (
-        <DashboardChartCard title='Progress Over Time' colSpan="col-span-2">
-            <DashboardChartProgressOverTimeClient data={totalWeightLiftedOverTime} />
-        </DashboardChartCard>
-    )
+    return {
+      period,
+      totalWeight: lastCumulativeWeight,
+    };
+  });
+
+  return <DashboardChartProgressOverTimeClient data={adjustedData} />;
 }
