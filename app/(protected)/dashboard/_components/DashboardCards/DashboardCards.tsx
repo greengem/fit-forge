aff50186aa9fee26cfa50eb3a7db14284919ea17
Green Gem <chris_waitt@me.com>
2024-02-17 10:20:00 +0000

    Update grid layout in DashboardCards component

--- app/(protected)/dashboard/_components/DashboardCards/DashboardCards.tsx
+++ app/(protected)/dashboard/_components/DashboardCards/DashboardCards.tsx
@@ -8,7 +8,7 @@ import {Spinner} from "@nextui-org/spinner";
 
 export default function DashboardCards() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 mb-5">
+        <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-5 mb-5">
             <Suspense fallback={<DashboardCardTemplate title="Weekly Workouts"><Spinner color="primary" /></DashboardCardTemplate>}>
                 <DashboardCardWeeklyWorkouts />
             </Suspense>
