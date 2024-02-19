ff0bf92d684cc16cadd1181e107b5da06aa1d97f
Green Gem <chris_waitt@me.com>
2024-02-19 13:20:35 +0000

    Delete SidebarToggle component

--- components/Navbar/SidebarToggle.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { Button } from "@nextui-org/react";
-import { useSidebarVisibility } from "@/contexts/SidebarContext";
-import { IconMenu2 } from "@tabler/icons-react";
-
-export default function SidebarToggle() {
-    const { toggleSidebar } = useSidebarVisibility();
-    return <Button isIconOnly variant="light" onPress={toggleSidebar} color="primary"><IconMenu2 /></Button>;
-}
