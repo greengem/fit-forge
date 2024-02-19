7dafb066ae540b0dec0b778aa8add81b14f0ac73
Green Gem <chris_waitt@me.com>
2024-02-19 13:04:17 +0000

    New exercise filter to show only exercises the user can perform with the equipment they own/have access to

---
+++ app/(protected)/exercises/_components/Filters/ExerciseFilterUserEquipment.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { Checkbox } from "@nextui-org/react";
+
+export default function ExerciseFilterUserEquipment() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    function handleFilterUserEquipment(event: React.ChangeEvent<HTMLInputElement>) {
+        const isFavourite = event.target.checked;
+        const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
+        if (isFavourite) {
+            params.set('equipmentOwned', 'true');
+        } else {
+            params.delete('equipmentOwned');
+        }
+        replace(`${pathname}?${params.toString()}`, { scroll: false });
+    }
+    
+    return (
+        <Checkbox 
+            size="sm" 
+            onChange={handleFilterUserEquipment}
+            defaultSelected={searchParams.get('equipmentOwned') === 'true'}
+        >
+            Exercises with my equipment
+        </Checkbox>
+    )
+}
