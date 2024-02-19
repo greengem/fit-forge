e07424bbed2521c84c34a5d4f1e235bf4ca3ac0e
Green Gem <chris_waitt@me.com>
2024-02-19 13:05:43 +0000

    Moved exercise detail modal and updated import paths

--- app/(protected)/exercises/_components/ExerciseInfoButton.tsx
+++ app/(protected)/exercises/_components/ExerciseInfoButton.tsx
@@ -2,11 +2,11 @@
 import { useContext } from "react";
 import { Button } from "@nextui-org/button"
 import { IconInfoCircle } from "@tabler/icons-react"
-import { ExerciseModalContext } from "@/contexts/ExerciseModalContext";
+import { ExerciseDetailModalContext } from "@/contexts/ExerciseDetailModalContext";
 import { Exercise } from "@prisma/client";
 
 export default function ExerciseInfoButton({ exercise } : { exercise: Exercise }) {
-    const { setExercise, onOpen } = useContext(ExerciseModalContext);
+    const { setExercise, onOpen } = useContext(ExerciseDetailModalContext);
 
     const handleClick = () => {
         setExercise(exercise);
---
+++ contexts/ExerciseDetailModalContext.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React, { ReactNode, useState } from 'react';
+import { useDisclosure } from "@nextui-org/use-disclosure";
+import { Exercise } from "@prisma/client";
+
+
+// Define the context value type
+interface ExerciseDetailModalContextType {
+  exercise: Exercise | null;
+  setExercise: (exercise: Exercise | null) => void;
+  isOpen: boolean;
+  onOpen: () => void;
+  onOpenChange: (isOpen: boolean) => void;
+}
+
+
+export const ExerciseDetailModalContext = React.createContext<ExerciseDetailModalContextType>({
+  exercise: null,
+  setExercise: () => {},
+  isOpen: false,
+  onOpen: () => {},
+  onOpenChange: () => {},
+});
+
+// Create the provider
+export function ExerciseDetailModalProvider({ children }: { children: ReactNode }) {
+  const [exercise, setExercise] = useState<Exercise | null>(null);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <ExerciseDetailModalContext.Provider value={{ exercise, setExercise, isOpen, onOpen, onOpenChange }}>
+      {children}
+    </ExerciseDetailModalContext.Provider>
+  );
+};
