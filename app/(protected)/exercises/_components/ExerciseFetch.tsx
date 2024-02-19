98f5a7a83997734dccf73f06ae75e17a70df5339
Green Gem <chris_waitt@me.com>
2024-02-19 13:06:26 +0000

    Update exercise fetch to use our new search params

--- app/(protected)/exercises/_components/ExerciseFetch.tsx
+++ app/(protected)/exercises/_components/ExerciseFetch.tsx
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs";
 import prisma from "@/prisma/prisma";
 import ExerciseTable from "./ExerciseTable";
 import { CategoryType, Muscle, LevelType, ForceType } from "@prisma/client";
-import ExercisePagination from "./ExercisePagination";
+import ExercisePagination from "./Filters/ExercisePagination";
 
 interface UserRoutine {
     id: string;
@@ -11,9 +11,25 @@ interface UserRoutine {
 }
 
 export default async function ExerciseFetch({ 
-    search, cat, muscle, level, force, currentPage, userRoutines
+    search, 
+    cat, 
+    muscle, 
+    level, 
+    force, 
+    currentPage, 
+    userRoutines, 
+    favs, 
+    equipmentOwned
 } : { 
-    search: string, cat: string[], muscle: string[], level: string[], force: string[], currentPage: number, userRoutines: UserRoutine[]
+    search: string, 
+    cat: string[], 
+    muscle: string[], 
+    level: string[], 
+    force: string[], 
+    currentPage: number, 
+    userRoutines: UserRoutine[], 
+    favs: boolean, 
+    equipmentOwned: boolean
 }) {
     const { userId } : { userId: string | null } = auth();
   
@@ -21,8 +37,19 @@ export default async function ExerciseFetch({
       throw new Error('You must be signed in to view this page.');
     }
 
+    const userEquipment = await prisma.userEquipment.findMany({
+        where: {
+            userId: userId,
+        },
+        select: {
+            equipmentType: true,
+        }
+    });
+
+    const userEquipmentOwned = userEquipment.map(equipment => equipment.equipmentType);
+
     const itemsPerPage = 10;
-    
+
     const exercises = await prisma.exercise.findMany({
         take: itemsPerPage,
         skip: (currentPage - 1) * itemsPerPage,
@@ -34,18 +61,26 @@ export default async function ExerciseFetch({
             category: cat.length > 0 ? { in: cat as CategoryType[] } : undefined,
             level: level.length > 0 ? { in: level as LevelType[] } : undefined,
             force: force.length > 0 ? { in: force as ForceType[] } : undefined,
+            equipment: equipmentOwned ? { in: userEquipmentOwned } : undefined,
             ...(muscle.length > 0 ? {
                 OR: [
                     { primary_muscles: { hasEvery: muscle as Muscle[] } },
                     { secondary_muscles: { hasEvery: muscle as Muscle[] } }
                 ]
+            } : {}),
+            ...(favs ? {
+                favouritedBy: {
+                    some: {
+                        userId: userId
+                    }
+                }
             } : {})
         },
         orderBy: {
             name: 'asc',
         }
     });
-
+    
     const numberOfResults = await prisma.exercise.count({
         where: {
             name: {
@@ -55,11 +90,19 @@ export default async function ExerciseFetch({
             category: cat.length > 0 ? { in: cat as CategoryType[] } : undefined,
             level: level.length > 0 ? { in: level as LevelType[] } : undefined,
             force: force.length > 0 ? { in: force as ForceType[] } : undefined,
+            equipment: equipmentOwned ? { in: userEquipmentOwned } : undefined,
             ...(muscle.length > 0 ? {
                 OR: [
                     { primary_muscles: { hasEvery: muscle as Muscle[] } },
                     { secondary_muscles: { hasEvery: muscle as Muscle[] } }
                 ]
+            } : {}),
+            ...(favs ? {
+                favouritedBy: {
+                    some: {
+                        userId: userId
+                    }
+                }
             } : {})
         },
     });
