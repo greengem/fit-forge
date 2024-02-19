3982a98bccd887b1e129746e4ef6caf33138fc2a
Green Gem <chris_waitt@me.com>
2024-02-19 13:07:18 +0000

    Users can now add exercises to routines directly from the exercises page!

--- app/(protected)/exercises/_components/ExerciseAddToRoutineButton.tsx
+++ app/(protected)/exercises/_components/ExerciseAddToRoutineButton.tsx
@@ -1,12 +1,9 @@
-'use client'
-import { Exercise } from "@prisma/client"
-import { User } from "@nextui-org/user";
-import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/popover"
-import { Button } from "@nextui-org/button"
-import { IconPlus } from "@tabler/icons-react"
-import { Input } from "@nextui-org/input";
-import { Divider } from "@nextui-org/divider";
-
+'use client';
+import { useContext } from "react";
+import { ExerciseAddToRoutineModalContext } from "@/contexts/ExerciseAddToRoutineModalContext";
+import { Exercise } from "@prisma/client";
+import { Button } from "@nextui-org/button";
+import { IconPlus } from "@tabler/icons-react";
 
 interface UserRoutine {
     id: string;
@@ -19,44 +16,14 @@ type ExerciseAddToRoutineButtonProps = {
     userRoutines: UserRoutine[];
 }
 
-export default function ExerciseAddToRoutineButton({ exercise, userRoutines } : ExerciseAddToRoutineButtonProps) {
-    return (
-        <Popover placement="left" showArrow={true}>
-            <PopoverTrigger><Button isIconOnly isDisabled><IconPlus size={20} /></Button></PopoverTrigger>
-            <PopoverContent>
-                <div className="px-2 pt-2 pb-1 min-w-64">
-                    <h4 className="mb-5 text-lg">Add <span className="font-semibold">{exercise.name}</span> to a Routine</h4>
+export default function ExerciseAddToRoutineButton({ exercise, userRoutines }: ExerciseAddToRoutineButtonProps) {
+    const { setExercise, setUserRoutines, onOpen } = useContext(ExerciseAddToRoutineModalContext);
 
-                    <h5 className="font-semibold mb-2">New Routine</h5>
+    const handleClick = () => {
+        setExercise(exercise);
+        setUserRoutines(userRoutines);
+        onOpen();
+      };
 
-                    <div className="flex items-center gap-3 justify-between">
-                        <form>
-                            <Input
-                                placeholder="My awesome workout plan"
-                                labelPlacement="outside"
-                            >
-                            </Input>
-                        </form>
-                        <div className="shrink-0"><Button size="md" isIconOnly><IconPlus size={18} /></Button></div>
-                    </div>
-
-                    <Divider className="my-2" />
-
-                    <h5 className="font-semibold mb-2">Existing Routines</h5>
-
-                    <ul>
-                        {userRoutines.map((routine) => (
-                            <li key={routine.id} className="space-y-2 flex gap-x-2 justify-between items-center">
-                                <User   
-                                    name={routine.name}
-                                    description={`${routine.exerciseCount} Exercises`}
-                                />
-                                <div><Button size="md" variant="flat" isIconOnly><IconPlus size={18} /></Button></div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </PopoverContent>
-        </Popover>
-    )
+      return <Button isIconOnly onClick={handleClick}><IconPlus size={20} /></Button>;
 }
---
+++ contexts/ExerciseAddToRoutineModalContext.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React, { ReactNode, useState } from 'react';
+import { useDisclosure } from "@nextui-org/use-disclosure";
+import { Exercise } from "@prisma/client";
+
+interface UserRoutine {
+  id: string;
+  name: string;
+  exerciseCount: number;
+}
+
+interface ExerciseAddToRoutineModalContextType {
+  exercise: Exercise | null;
+  setExercise: (exercise: Exercise | null) => void;
+  isOpen: boolean;
+  onOpen: () => void;
+  onOpenChange: (isOpen: boolean) => void;
+  userRoutines: UserRoutine[];
+  setUserRoutines: (userRoutines: UserRoutine[]) => void;
+}
+
+
+export const ExerciseAddToRoutineModalContext = React.createContext<ExerciseAddToRoutineModalContextType>({
+  exercise: null,
+  setExercise: () => {},
+  isOpen: false,
+  onOpen: () => {},
+  onOpenChange: () => {},
+  userRoutines: [],
+  setUserRoutines: () => {},
+});
+
+// Create the provider
+export function ExerciseAddToRoutineModalProvider({ children }: { children: ReactNode }) {
+  const [exercise, setExercise] = useState<Exercise | null>(null);
+  const [userRoutines, setUserRoutines] = useState<UserRoutine[]>([]);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <ExerciseAddToRoutineModalContext.Provider value={{ exercise, setExercise, isOpen, onOpen, onOpenChange, userRoutines, setUserRoutines }}>
+      {children}
+    </ExerciseAddToRoutineModalContext.Provider>
+  );
+};
