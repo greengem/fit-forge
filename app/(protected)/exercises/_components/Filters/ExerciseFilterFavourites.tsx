c65240da4973a63f6c9dc877375dc1632d6408ca
Green Gem <chris_waitt@me.com>
2024-02-19 13:03:55 +0000

    New exercise filter to only show user favourites

---
+++ app/(protected)/exercises/_components/Filters/ExerciseFilterFavourites.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { Checkbox } from "@nextui-org/react";
+
+export default function ExerciseFilterFavourites() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    function handleFilterFavourites(event: React.ChangeEvent<HTMLInputElement>) {
+        const isFavourite = event.target.checked;
+        const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
+        if (isFavourite) {
+            params.set('favs', 'true');
+        } else {
+            params.delete('favs');
+        }
+        replace(`${pathname}?${params.toString()}`, { scroll: false });
+    }
+    
+    return (
+        <Checkbox 
+            size="sm" 
+            onChange={handleFilterFavourites}
+            defaultSelected={searchParams.get('favs') === 'true'}
+        >
+            My Favourites
+        </Checkbox>
+    )
+}
