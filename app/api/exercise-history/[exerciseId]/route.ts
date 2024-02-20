ba563490649b3792fbd13325e0f7df19895d11ed
Green Gem <chris_waitt@me.com>
2024-02-20 15:03:21 +0000

    Add route for fetching exercise history in modals

---
+++ app/api/exercise-history/[exerciseId]/route.ts
@@ -0,0 +1,45 @@
+import { auth } from "@clerk/nextjs";
+import prisma from '@/prisma/prisma';
+
+export async function GET(request: Request, { params }: { params: { exerciseId: string } }) {
+    try {
+        const { userId } : { userId: string | null } = await auth();
+        const { exerciseId } = params;
+
+        console.log('exerciseId:', exerciseId);
+
+        if (!userId) {
+            return new Response(JSON.stringify({ error: "User not authenticated" }), { status: 401 });
+        }
+
+        const history = await prisma.workoutLog.findMany({
+            where: {
+                userId: userId,
+                exercises: {
+                    some: {
+                        exerciseId: exerciseId,
+                    },
+                },
+            },
+            include: {
+                exercises: {
+                    where: {
+                        exerciseId: exerciseId,
+                    },
+                    include: {
+                        sets: true,
+                    },
+                },
+            },
+            orderBy: {
+                date: 'desc',
+            },
+        });
+
+        return new Response(JSON.stringify(history), { status: 200 });
+        
+    } catch (error) {
+        console.error('Error details:', error);
+        return new Response(JSON.stringify({ error: "Error fetching exercise history" }), { status: 500 });
+    }
+}
