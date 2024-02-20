dd6fc5feab437d670587aaf0d1a6b7a23a2a7dcc
Green Gem <chris_waitt@me.com>
2024-02-20 17:20:24 +0000

    Update ExerciseDetailModal component

--- app/(protected)/exercises/_components/Modals/ExerciseDetailModal/ExerciseDetailModal.tsx
+++ app/(protected)/exercises/_components/Modals/ExerciseDetailModal/ExerciseDetailModal.tsx
@@ -1,14 +1,13 @@
 'use client';
 import { useContext } from "react";
 import { ExerciseDetailModalContext } from "@/contexts/ExerciseDetailModalContext";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@nextui-org/modal";
+import { Modal, ModalContent, ModalHeader, ModalBody } from "@nextui-org/modal";
 import { Tabs, Tab } from "@nextui-org/tabs";
 import { IconChartBar, IconHistory, IconInfoCircle, IconTrophy } from "@tabler/icons-react";
 import AboutTab from "./AboutTab";
 import HistoryTab from "./HistoryTab";
 import ChartsTab from "./ChartsTab";
 import RecordsTab from "./RecordsTab";
-
 
 export default function ExerciseDetailModal() {
   const { exercise, isOpen, onOpenChange } = useContext(ExerciseDetailModalContext);
@@ -25,35 +24,34 @@ export default function ExerciseDetailModal() {
                   key="about" 
                   title={
                     <div className="flex items-center space-x-2">
-                      <IconInfoCircle size={18} />
+                      <IconInfoCircle size={17} />
                       <span>About</span>
                     </div>
                   }><AboutTab exercise={exercise} /></Tab>
  
                 <Tab 
-                  isDisabled
                   key="history" 
                   title={
                     <div className="flex items-center space-x-2">
-                      <IconHistory size={18} />
+                      <IconHistory size={17} />
                       <span>History</span>
                     </div>
-                  }><HistoryTab /></Tab>
+                  }><HistoryTab exerciseId={exercise?.id} /></Tab>
                 <Tab 
                   isDisabled
                   key="charts" 
                   title={
                     <div className="flex items-center space-x-2">
-                      <IconChartBar size={18} />
+                      <IconChartBar size={17} />
                       <span>Charts</span>
                     </div>
-                  }><ChartsTab exerciseName={exercise?.name} /></Tab>
+                  }><ChartsTab exerciseId={exercise?.id} exerciseName={exercise?.name} /></Tab>
                 <Tab 
                   isDisabled
                   key="records" 
                   title={
                     <div className="flex items-center space-x-2">
-                      <IconTrophy size={18} />
+                      <IconTrophy size={17} />
                       <span>Records</span>
                     </div>
                   }><RecordsTab /></Tab>
