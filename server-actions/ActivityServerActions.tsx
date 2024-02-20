d30c291b47ce6888bfb8ea8d781b94c1fbad0fca
Green Gem <chris_waitt@me.com>
2024-02-20 17:19:03 +0000

    add try catch and cache invalidation to delete activity server action

--- server-actions/ActivityServerActions.tsx
+++ server-actions/ActivityServerActions.tsx
@@ -1,10 +1,17 @@
 'use server'
 import prisma from "@/prisma/prisma";
+import { revalidatePath } from "next/cache";
 
 export async function handleDeleteActivity(activityId: string) {
-    await prisma.workoutLog.delete({
-        where: {
-            id: activityId,
-        },
-    });
+    try {
+        await prisma.workoutLog.delete({
+            where: {
+                id: activityId,
+            },
+        });
+        revalidatePath('/activity');
+        return { success: true, message: 'Activity deleted successfully' };
+    } catch (e) {
+        return { success: false, message: 'Failed to delete activity' };
+    }
 }
