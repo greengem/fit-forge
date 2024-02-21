cbdcb0639ba9cdd8aaf26e958f3879dc8445d4cc
Green Gem <chris_waitt@me.com>
2024-02-21 20:35:30 +0000

    Add search functionality to admin dashboard

---
+++ app/(protected)/admin/dashboard/_search-users.tsx
@@ -0,0 +1,34 @@
+"use client";
+ 
+import { usePathname, useRouter } from "next/navigation";
+import { Input } from "@nextui-org/input"; 
+import { Button } from "@nextui-org/button";
+
+export const SearchUsers = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+ 
+  return (
+    <div>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const form = e.currentTarget;
+          const formData = new FormData(form);
+          const queryTerm = formData.get("search") as string;
+          router.push(pathname + "?search=" + queryTerm);
+        }}
+      >
+        <Input 
+          label="Search for Users" 
+          placeholder="John Doe..."
+          id="search" 
+          name="search" 
+          type="text" 
+          className="mb-3"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
+};
