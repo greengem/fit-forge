c24a762e7cdf9fc341eb6bd2e85b9c53b776b14b
Green Gem <chris_waitt@me.com>
2024-02-21 20:35:39 +0000

    Add role-based authorization utility function

---
+++ utils/roles.ts
@@ -0,0 +1,8 @@
+import { Roles } from "@/types/globals"
+import { auth } from "@clerk/nextjs"
+ 
+export const checkRole = (role: Roles) => {
+  const { sessionClaims } = auth()
+ 
+  return sessionClaims?.metadata.role === role;
+}
