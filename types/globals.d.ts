c904ec8a5c6941f7630b150ec1e143e1c5ec057e
Green Gem <chris_waitt@me.com>
2024-02-21 20:35:36 +0000

    Add global type declaration for CustomJwtSessionClaims

---
+++ types/globals.d.ts
@@ -0,0 +1,13 @@
+export { };
+ 
+// Create a type for the roles
+export type Roles = "admin" | "moderator";
+ 
+declare global {
+  interface CustomJwtSessionClaims {
+    metadata: {
+      role?: Roles,
+      onboardingComplete?: boolean
+    };
+  }
+}
