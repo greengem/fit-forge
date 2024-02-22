11c7032cf73c84030c294114dcc5de6d659006af
Green Gem <chris_waitt@me.com>
2024-02-22 17:37:29 +0000

    Add user authentication to Sidebar component

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { currentUser } from "@clerk/nextjs";
 import SidebarNav from "./SidebarNav";
 import SidebarBrand from "./SidebarBrand";
 import { ThemeSwitcher } from "@/components/ThemeSwitcher/ThemeSwitcher";
@@ -5,20 +6,29 @@ import SidebarWorkoutControls from "./SidebarWorkoutControls";
 import SidebarUser from "./SidebarUser";
 import SidebarSearch from "./SidebarSearch";
 import SidebarSocials from "./SidebarSocials";
+import SidebarWrapper from "./SidebarWrapper.client";
 
-export default function Sidebar() {
+export default async function Sidebar() {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("You must be signed in to view this page.");
+  }
+
+  const username = user?.username || undefined;
+  const userImage = user?.imageUrl;
+
   return (
-    <aside className="fixed top-0 left-0 h-full w-64 dark:bg-zinc-900 hidden md:block shadow-md">
+    <SidebarWrapper>
       <SidebarBrand />
-      <SidebarUser />
+      <SidebarUser username={username} userImage={userImage} />
       {/* <SidebarSearch /> */}
       <SidebarNav />
       <SidebarWorkoutControls />
-
       <div className="absolute bottom-0 left-0 right-0 py-5 px-5 flex flex-col items-center">
         <ThemeSwitcher />
         {/* <SidebarSocials /> */}
       </div>
-    </aside>
+    </SidebarWrapper>
   );
 }
