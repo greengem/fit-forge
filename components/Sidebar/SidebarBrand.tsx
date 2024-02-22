c12ee808d16f41b83ae857fda09c973f0a1a8298
Green Gem <chris_waitt@me.com>
2024-02-22 22:24:00 +0000

    Update SidebarBrand component to dynamically apply flex justify-center class when sidebar is collapsed

--- components/Sidebar/SidebarBrand.tsx
+++ components/Sidebar/SidebarBrand.tsx
@@ -3,12 +3,13 @@ import { useSidebarToggleContext } from '@/contexts/SidebarToggleContext';
 import { Chip } from "@nextui-org/react";
 import { IconBarbell } from "@tabler/icons-react";
 import Link from "next/link";
+import clsx from 'clsx';
 
 export default function SidebarBrand() {
   const { sidebarCollapse } = useSidebarToggleContext();
 
   return (
-    <div className="px-5 mb-6">
+    <div className={clsx("px-5 mb-6", sidebarCollapse ? "flex justify-center" : "")}>
       <Link href="/" className="flex items-center gap-3">
         <div className="flex items-end justify-center w-11 h-[60px] pb-2 bg-primary rounded-b-lg">
           <IconBarbell stroke={1} size={28} className="text-black" />
