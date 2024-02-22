6b00cf1389de1ca980c77f5ec87c05c7c0cc770b
Green Gem <chris_waitt@me.com>
2024-02-22 17:37:15 +0000

    Add SidebarWrapper component

---
+++ components/Sidebar/SidebarWrapper.client.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { useSidebarToggleContext } from '@/contexts/SidebarToggleContext';
+import clsx from 'clsx';
+
+export default function SidebarWrapper({ children }: { children: React.ReactNode }) {
+    const { sidebarCollapse, toggleSidebar } = useSidebarToggleContext();
+
+    const sidebarClass = clsx({
+      'fixed top-0 left-0 h-full dark:bg-zinc-900 hidden md:block shadow-md overflow-hidden': true,
+      'w-20 sidebar-collapsed': sidebarCollapse,
+      'w-64 sidebar-expanded': !sidebarCollapse,
+    });
+
+    return (
+        <aside className={sidebarClass}>
+            {children}
+        </aside>
+    )
+}
