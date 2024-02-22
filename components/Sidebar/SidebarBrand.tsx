572bcf0f17f4acd25bb63c5cb3e9f0e1e9abec1c
Green Gem <chris_waitt@me.com>
2024-02-22 11:57:39 +0000

    Update SidebarBrand component

--- components/Sidebar/SidebarBrand.tsx
+++ components/Sidebar/SidebarBrand.tsx
@@ -1,5 +1,5 @@
 import { Chip } from "@nextui-org/react";
-import { IconBarbell, IconHammer } from "@tabler/icons-react";
+import { IconBarbell } from "@tabler/icons-react";
 import Link from "next/link";
 
 export default function SidebarBrand() {
@@ -7,10 +7,10 @@ export default function SidebarBrand() {
     <div className="px-5 mb-6">
       <Link href="/" className="flex items-center gap-3">
         <div className="flex items-end justify-center w-11 h-[60px] pb-2 bg-primary rounded-b-lg">
-          <IconHammer stroke={1} size={28} className="text-black" />
+          <IconBarbell stroke={1} size={28} className="text-black" />
         </div>
         <div className="flex gap-3 items-center pt-[18px]">
-          <p className="text-xl">FitForge</p>
+          <p className="text-xl">Tracktive</p>
           {/* <Chip color="primary" radius="full">Pro</Chip> */}
         </div>
       </Link>
