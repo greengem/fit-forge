9b686e68a7e924a872072c6186b33ac31b8c1cc0
Green Gem <chris_waitt@me.com>
2024-02-22 17:37:39 +0000

    Add sidebar collapse functionality to SidebarBrand component

--- components/Sidebar/SidebarBrand.tsx
+++ components/Sidebar/SidebarBrand.tsx
@@ -1,18 +1,24 @@
+'use client'
+import { useSidebarToggleContext } from '@/contexts/SidebarToggleContext';
 import { Chip } from "@nextui-org/react";
 import { IconBarbell } from "@tabler/icons-react";
 import Link from "next/link";
 
 export default function SidebarBrand() {
+  const { sidebarCollapse } = useSidebarToggleContext();
+
   return (
     <div className="px-5 mb-6">
       <Link href="/" className="flex items-center gap-3">
         <div className="flex items-end justify-center w-11 h-[60px] pb-2 bg-primary rounded-b-lg">
           <IconBarbell stroke={1} size={28} className="text-black" />
         </div>
-        <div className="flex gap-3 items-center pt-[18px]">
-          <p className="text-xl">Tracktive</p>
-          {/* <Chip color="primary" radius="full">Pro</Chip> */}
-        </div>
+        {!sidebarCollapse &&
+          <div className="flex gap-3 items-center pt-[18px]">
+            <p className="text-xl">Tracktive</p>
+            {/* <Chip color="primary" radius="full">Pro</Chip> */}
+          </div>
+        }
       </Link>
     </div>
   );
