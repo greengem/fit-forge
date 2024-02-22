a124ebe80d27ec42aa7d315e6554f767814d5393
Green Gem <chris_waitt@me.com>
2024-02-22 17:37:49 +0000

    Add social media buttons to Sidebar

--- components/Sidebar/SidebarSocials.tsx
+++ components/Sidebar/SidebarSocials.tsx
@@ -1,29 +1,30 @@
+import Link from "next/link";
+import { Button } from "@nextui-org/button";
 import {
   IconBrandFacebook,
   IconBrandInstagram,
   IconBrandTiktok,
   IconBrandX,
 } from "@tabler/icons-react";
-import Link from "next/link";
 
 export default function SidebarSocials() {
   return (
     <div className="w-full grid grid-cols-4 text-primary">
-      <Link href="https://www.facebook.com/" className="flex justify-center">
+      <Button size="sm" color="primary" variant="ghost" isIconOnly as={Link} href="https://www.facebook.com/" className="flex justify-center">
         <IconBrandTiktok />
-      </Link>
+      </Button>
 
-      <Link href="https://www.facebook.com/" className="flex justify-center">
+      <Button size="sm" color="primary" variant="ghost" isIconOnly as={Link} href="https://www.facebook.com/" className="flex justify-center">
         <IconBrandX />
-      </Link>
+      </Button>
 
-      <Link href="https://www.facebook.com/" className="flex justify-center">
+      <Button size="sm" color="primary" variant="ghost" isIconOnly as={Link} href="https://www.facebook.com/" className="flex justify-center">
         <IconBrandInstagram />
-      </Link>
+      </Button>
 
-      <Link href="https://www.facebook.com/" className="flex justify-center">
+      <Button size="sm" color="primary" variant="ghost" isIconOnly as={Link} href="https://www.facebook.com/" className="flex justify-center">
         <IconBrandFacebook />
-      </Link>
+      </Button>
     </div>
   );
 }
