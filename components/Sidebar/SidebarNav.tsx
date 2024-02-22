8de597b0cf7323112432c6664994707039ab4856
Green Gem <chris_waitt@me.com>
2024-02-22 11:36:49 +0000

    Refactor SidebarNav component

--- components/Sidebar/SidebarNav.tsx
+++ components/Sidebar/SidebarNav.tsx
@@ -1,15 +1,17 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { SignOutButton } from "@clerk/nextjs";
+import clsx from "clsx";
+
 import {
   IconDashboard,
   IconJumpRope,
   IconStretching,
-  IconUser,
   IconActivity,
-  IconLogout,
-  IconPlus,
+  IconUser,
+  IconBook2,
+  IconBook,
+  IconHelp,
 } from "@tabler/icons-react";
 import { Divider } from "@nextui-org/divider";
 
@@ -17,52 +19,61 @@ export default function SidebarNav() {
   const pathname = usePathname();
 
   return (
-    <ul className="space-y-3 text-sm">
-      <Divider />
-      <li className="uppercase text-xs text-gray-500 font-bold">Data</li>
-      <NavItem
-        icon={<IconDashboard className="h-5 w-5" />}
-        label="Dashboard"
-        href="/dashboard"
-        active={pathname === "/dashboard"}
-      />
-      <NavItem
-        icon={<IconActivity className="h-5 w-5" />}
-        label="Activity"
-        href="/activity"
-        active={pathname === "/activity"}
-      />
-      <Divider />
-      <li className="uppercase text-xs text-gray-500 font-bold">Workout</li>
-      <NavItem
-        icon={<IconJumpRope className="h-5 w-5" />}
-        label="Start Workout"
-        href="/workout"
-        active={pathname.startsWith("/workout")}
-      />
-      <NavItem
-        icon={<IconStretching className="h-5 w-5" />}
-        label="Browse Exercises"
-        href="/exercises"
-        active={pathname === "/exercises"}
-      />
-      <Divider />
-      <li className="uppercase text-xs text-gray-500 font-bold">User</li>
-      <NavItem
-        icon={<IconUser className="h-5 w-5" />}
-        label="Profile"
-        href="/profile"
-        active={pathname === "/profile"}
-      />
-      <li>
-        <SignOutButton>
-          <button className="flex items-center gap-3">
-            <IconLogout className="h-5 w-5" />
-            Sign Out
-          </button>
-        </SignOutButton>
-      </li>
-    </ul>
+    <div className="px-5">
+      <ul className="text-sm">
+        <li className="uppercase text-xs text-gray-500 font-semibold mb-1 px-2">Data</li>
+        <div className="space-y-1 mb-4">
+          <NavItem
+            icon={<IconDashboard size={22} />}
+            label="Dashboard"
+            href="/dashboard"
+            active={pathname === "/dashboard"}
+          />
+          <NavItem
+            icon={<IconActivity size={22} />}
+            label="Activity"
+            href="/activity"
+            active={pathname === "/activity"}
+          />
+          <NavItem
+            icon={<IconUser size={22} />}
+            label="Profile"
+            href="/profile"
+            active={pathname === "/profile"}
+          />
+        </div>
+
+        <li className="uppercase text-xs text-gray-500 font-semibold mb-1 px-2">Workout</li>
+        <div className="space-y-1 mb-4">
+          <NavItem
+            icon={<IconJumpRope size={22} />}
+            label="Start Workout"
+            href="/workout"
+            active={pathname.startsWith("/workout")}
+          />
+          <NavItem
+            icon={<IconStretching size={22} />}
+            label="Browse Exercises"
+            href="/exercises"
+            active={pathname === "/exercises"}
+          />
+        </div>
+
+        <li className="uppercase text-xs text-gray-500 font-semibold mb-1 px-2">More</li>
+        <NavItem
+          icon={<IconBook size={22} />}
+          label="Docs"
+          href="/docs"
+          active={pathname === "/docs"}
+        />
+        <NavItem
+          icon={<IconHelp size={22} />}
+          label="Support"
+          href="/support"
+          active={pathname === "/support"}
+        />
+      </ul>
+    </div>
   );
 }
 
@@ -84,13 +95,13 @@ function NavItem({
   children,
 }: NavItemProps) {
   const content = (
-    <div
-      className={`flex items-center space-x-3 ${active && "text-primary"} text-foreground`}
-    >
-      {icon}
-      {label && <div>{label}</div>}
-    </div>
-  );
+<div
+  className={clsx('flex items-center space-x-3 p-2 rounded-lg', active ? 'bg-zinc-300 dark:bg-zinc-800 text-black dark:text-white' : 'text-zinc-600 dark:text-zinc-400')}
+>
+  {icon}
+  {label && <div>{label}</div>}
+</div>
+);
 
   return (
     <li>
