c4f9218ad959aea08bdf8be414dd1f48f3f3c749
Green Gem <chris_waitt@me.com>
2024-02-22 17:36:50 +0000

    Add SidebarToggleContext component

---
+++ contexts/SidebarToggleContext.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React, { useState, useContext } from 'react';
+
+interface SidebarToggleContextProps {
+  sidebarCollapse: boolean;
+  toggleSidebar: () => void;
+}
+
+const SidebarToggleContext = React.createContext<SidebarToggleContextProps | undefined>(undefined);
+
+export const useSidebarToggleContext = () => {
+  const context = useContext(SidebarToggleContext);
+  if (!context) {
+    throw new Error('useSidebarToggleContext must be used within a SidebarToggleProvider');
+  }
+  return context;
+};
+
+export function SidebarToggleProvider({ children }: { children: React.ReactNode }) {
+  const [sidebarCollapse, setSidebarCollapse] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarCollapse(!sidebarCollapse);
+  };
+
+  return (
+    <SidebarToggleContext.Provider value={{ sidebarCollapse, toggleSidebar }}>
+      {children}
+    </SidebarToggleContext.Provider>
+  );
+};
