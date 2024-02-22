edc673fb0bc1078dfd1b7841b5897569b29dc131
Green Gem <chris_waitt@me.com>
2024-02-22 11:36:40 +0000

    Refactor Sidebar component

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -2,22 +2,16 @@ import SidebarNav from "./SidebarNav";
 import SidebarBrand from "./SidebarBrand";
 import { ThemeSwitcher } from "@/components/ThemeSwitcher/ThemeSwitcher";
 import SidebarWorkoutControls from "./SidebarWorkoutControls";
+import SidebarUser from "./SidebarUser";
+import SidebarSearch from "./SidebarSearch";
 
 export default function Sidebar() {
   return (
-    <aside
-      className="
-            fixed top-0 left-0 h-full w-64 
-            group-light-bg-1 group-dark-bg-1
-            hidden md:block 
-            shadow-md"
-    >
-      <div>
-        <SidebarBrand />
-      </div>
-      <div className="px-5 pt-3 mb-2">
-        <SidebarNav />
-      </div>
+    <aside className="fixed top-0 left-0 h-full w-64 dark:bg-zinc-900 hidden md:block shadow-md">
+      <SidebarBrand />
+      <SidebarUser />
+      {/* <SidebarSearch /> */}
+      <SidebarNav />
       <SidebarWorkoutControls />
 
       <div>
