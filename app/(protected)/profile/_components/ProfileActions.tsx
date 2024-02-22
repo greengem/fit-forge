2fc170a85bb4a10ff14d4d8885b43778b2014af3
Green Gem <chris_waitt@me.com>
2024-02-22 11:36:24 +0000

    Add logout icon to sign out button

--- app/(protected)/profile/_components/ProfileActions.tsx
+++ app/(protected)/profile/_components/ProfileActions.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { SignOutButton } from "@clerk/nextjs";
 import { Button } from "@nextui-org/button";
+import { IconLogout } from "@tabler/icons-react";
 
 export default function ProfileActions() {
   return (
     <SignOutButton>
-      <Button>Sign out</Button>
+      <Button><IconLogout /> Sign out</Button>
     </SignOutButton>
   );
 }
