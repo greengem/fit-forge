333f18c901aea912003080bab62e1036a7f9a2a0
Green Gem <chris_waitt@me.com>
2024-02-22 11:57:32 +0000

    Add new page component for documentation

---
+++ app/(protected)/docs/page.tsx
@@ -0,0 +1,11 @@
+import PageHeading from "@/components/PageHeading/PageHeading";
+
+export default function Docs() {
+    return (
+        <>
+            <PageHeading title="Documentation" />
+            <p>Coming soon</p>
+        </>
+
+    )
+}
