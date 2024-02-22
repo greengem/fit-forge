440922805624a9718fb8a59bc183f59f84bd397f
Green Gem <chris_waitt@me.com>
2024-02-22 22:23:56 +0000

    Update grid gap in DashboardCharts component

--- app/(protected)/dashboard/_components/DashboardCharts/DashboardCharts.tsx
+++ app/(protected)/dashboard/_components/DashboardCharts/DashboardCharts.tsx
@@ -24,7 +24,7 @@ export default function DashboardCharts({
   chart4DateRange?: string;
 }) {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-2 xl:grid-cols-6 gap-5 mb-5">
+    <div className="grid grid-cols-2 lg:grid-cols-2 xl:grid-cols-6 gap-3 mb-3">
       <DashboardChartCard
         icon={<IconChartBar />}
         title="Frequency"
