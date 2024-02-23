24b61ad35aa045b1a2e3fffcb56c795ced3df957
Green Gem <chris_waitt@me.com>
2024-02-23 09:51:12 +0000

    Add WorkoutTypes.ts file

---
+++ app/(protected)/workout/[id]/_components/WorkoutTypes.ts
@@ -0,0 +1,20 @@
+export interface Exercise {
+    id: string;
+    name: string;
+}
+
+export interface WorkoutPlanExercise {
+    Exercise: Exercise;
+    sets: number;
+    reps: number | null;
+    exerciseDuration: number | null;
+    trackingType: string;
+    order: number;
+}
+
+export interface Workout {
+    id: string;
+    name: string;
+    notes: string | null;
+    WorkoutPlanExercise: WorkoutPlanExercise[];
+}
