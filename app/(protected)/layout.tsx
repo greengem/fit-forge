fe10d72a762bc083b2ec78604374fcd5fb2177da
Green Gem <chris_waitt@me.com>
2024-02-23 18:59:12 +0000

    Add SiteNotice component to RootLayout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -3,6 +3,7 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 import Navbar from "@/components/Navbar/Navbar";
 import ActiveWorkoutWarning from "@/components/Notices/ActiveWorkoutWarning";
 import LayoutWrapper from "./LayoutWrapper.client";
+import SiteNotice from "./SiteNotice";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
+      <SiteNotice />
       <div className="flex">
         <Sidebar />
         <main className="flex-grow w-full">
