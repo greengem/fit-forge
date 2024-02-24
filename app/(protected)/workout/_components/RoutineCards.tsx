723ef9782e6f54dd30b0a2f603be240099608d67
Green Gem <chris_waitt@me.com>
2024-02-24 10:58:38 +0000

    Refactor RoutineCards component to use TypeScript

---
+++ app/(protected)/workout/_components/RoutineCards.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useState } from "react";
+import RoutineCard from "./RoutineCard";
+import { useWorkoutControls } from "@/contexts/WorkoutControlsContext";
+import { WorkoutPlan } from "@prisma/client";
+
+type Exercise = {
+  id: string;
+  name: string;
+  category: string;
+};
+
+type WorkoutPlanExercise = {
+  Exercise: Exercise;
+  order: number;
+  sets: number;
+};
+
+type ExtendedWorkoutPlan = WorkoutPlan & {
+  WorkoutPlanExercise: WorkoutPlanExercise[];
+};
+
+type RoutineCardsProps = {
+  routines: ExtendedWorkoutPlan[];
+  isSystem: boolean;
+};
+
+type ExpandedRoutines = {
+  [routineId: string]: boolean;
+};
+
+export default function RoutineCards({ routines, isSystem } : RoutineCardsProps) {
+
+  const { activeWorkoutRoutine } = useWorkoutControls();
+
+  const [expandedRoutines, setExpandedRoutines] = useState<ExpandedRoutines>({});
+
+  const toggleRoutineExpanded = (routineId: string) => {
+    setExpandedRoutines((prevState: ExpandedRoutines) => ({
+      ...prevState,
+      [routineId]: !prevState[routineId],
+    }));
+  };
+
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5">
+      {routines.map((routine) => (
+        <RoutineCard
+          key={routine.id}
+          routine={routine}
+          isSystem={isSystem}
+          isExpanded={expandedRoutines[routine.id] || false}
+          onToggleExpanded={toggleRoutineExpanded}
+          activeWorkoutRoutine={activeWorkoutRoutine}
+        />
+      ))}
+    </div>
+  );
+}
