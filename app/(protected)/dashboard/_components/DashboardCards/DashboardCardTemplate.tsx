c1eb24004d9a806ca997ee32d61f02a94f3f7ade
Green Gem <chris_waitt@me.com>
2024-02-27 13:55:42 +0000

    Add icon prop to DashboardCardTemplate

--- app/(protected)/dashboard/_components/DashboardCards/DashboardCardTemplate.tsx
+++ app/(protected)/dashboard/_components/DashboardCards/DashboardCardTemplate.tsx
@@ -2,13 +2,16 @@ import { Card, CardHeader, CardBody } from "@nextui-org/card";
 export default function DashboardCardTemplate({
   children,
   title,
+  icon,
 }: {
   children: React.ReactNode;
   title: string;
+  icon?: React.ReactNode;
 }) {
   return (
     <Card shadow="none" className="shadow-md">
-      <CardHeader className="px-3 text-xs uppercase block w-full truncate">
+      <CardHeader className="px-3 text-xs uppercase block w-full truncate flex gap-x-1">
+        {icon}
         {title}
       </CardHeader>
       <CardBody className="px-3 text-4xl pt-0 text-primary">
