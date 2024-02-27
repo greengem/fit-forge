6284afe7fc3c2ca1c3d3dadffef69320805e41d2
Green Gem <chris_waitt@me.com>
2024-02-27 13:54:22 +0000

    Refactor DashboardLinks component to use Card component for displaying links

--- app/(protected)/dashboard/_components/DashboardLinks.tsx
+++ app/(protected)/dashboard/_components/DashboardLinks.tsx
@@ -1,36 +1,35 @@
-import { Button } from "@nextui-org/button";
+'use client'
+import clsx from "clsx";
+import Image from "next/image";
 import Link from "next/link";
-import {
-  IconJumpRope,
-  IconList,
-  IconStretching,
-  IconUser,
-  IconActivity,
-} from "@tabler/icons-react";
+import { Card, CardHeader } from "@nextui-org/react";
 
 export default function DashboardLinks() {
   const links = [
-    { href: "/workout", icon: IconJumpRope, label: "Start a Workout" },
-    { href: "/workout/routine/new", icon: IconList, label: "Create a Routine" },
-    { href: "/exercises", icon: IconStretching, label: "Browse Exercises" },
-    { href: "/activity", icon: IconActivity, label: "View Activity" },
-    { href: "/profile", icon: IconUser, label: "Edit Profile" },
+    { title: "Exercises", description: "Browse Exercises", image: "02.webp", href: "/exercises", titleColour: "text-white/60", descriptionColour: "text-black"},
+    { title: "Routine", description: "Create a Routine", image: "24.webp", href: "/workout/routine/new", titleColour: "text-white/60", descriptionColour: "text-white" },
+    { title: "Workout", description: "Start a Workout", image: "21.webp", href: "/workout", titleColour: "text-white/60", descriptionColour: "text-white" },
+    { title: "Activity", description: "View Activity", image: "22.webp", href: "/activity", titleColour: "text-black/60", descriptionColour: "text-black" },
   ];
 
   return (
-    <div className="flex flex-wrap gap-x-3 gap-y-2 mb-3">
-      {links.map((link) => (
-        <Button
-          color="default"
-          variant="flat"
-          key={link.href}
-          as={Link}
-          href={link.href}
-          className="gap-unit-1"
-        >
-          <link.icon size={20} />
-          {link.label}
-        </Button>
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
+      {links.map((link, index) => (
+        <Link key={index} href={link.href}>
+          <Card key={index} className="h-[300px]" isPressable>
+            <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+              <p className={clsx("text-tiny uppercase font-bold", link.titleColour)}>{link.title}</p>
+              <h4 className={clsx("font-medium text-large", link.descriptionColour)}>{link.description}</h4>
+            </CardHeader>
+            <Image
+              alt="Card background"
+              className="z-0 w-full h-full object-cover"
+              src={`/card-images/${link.image}`}
+              width={800}
+              height={800}
+            />
+          </Card>
+        </Link>
       ))}
     </div>
   );
