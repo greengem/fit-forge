33adc76a4e52737c9f6716effd3cf3bf58fef280
Green Gem <chris_waitt@me.com>
2024-02-27 21:38:53 +0000

    Update DashboardLinks component styling

--- app/(protected)/dashboard/_components/DashboardLinks.tsx
+++ app/(protected)/dashboard/_components/DashboardLinks.tsx
@@ -9,14 +9,14 @@ export default function DashboardLinks() {
     { title: "Exercises", description: "Browse Exercises", image: "02.webp", href: "/exercises", titleColour: "text-white/60", descriptionColour: "text-danger"},
     { title: "Routine", description: "Create a Routine", image: "21.webp", href: "/workout/routine/new", titleColour: "text-white/60", descriptionColour: "text-white" },
     { title: "Workout", description: "Start a Workout", image: "e.webp", href: "/workout", titleColour: "text-white/60", descriptionColour: "text-primary" },
-    { title: "Activity", description: "View Activity", image: "22.webp", href: "/activity", titleColour: "text-black/60", descriptionColour: "text-black" },
+    { title: "Activity", description: "View Activity", image: "22.webp", href: "/activity", titleColour: "text-white/60", descriptionColour: "text-white" },
   ];
 
   return (
     <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
       {links.map((link, index) => (
         <Link key={index} href={link.href}>
-          <Card key={index} className="h-[300px] w-full" isPressable>
+          <Card key={index} className="w-full aspect-square" isPressable>
             <CardHeader className="absolute z-10 top-1 flex-col !items-start">
               <p className={clsx("text-xs uppercase font-bold", link.titleColour)}>{link.title}</p>
               <h4 className={clsx("font-medium text-lg", link.descriptionColour)}>{link.description}</h4>
