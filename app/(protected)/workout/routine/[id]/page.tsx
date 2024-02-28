fc82e685e45c82b9598f2bae76e0c67098bb02d8
Green Gem <chris_waitt@me.com>
2024-02-28 14:58:41 +0000

    Refactor NewRoutine Page to fetch existing routines and search results with SSR

--- app/(protected)/workout/routine/[id]/page.tsx
+++ app/(protected)/workout/routine/[id]/page.tsx
@@ -1,100 +1,31 @@
 import { auth } from "@clerk/nextjs";
 import PageHeading from "@/components/PageHeading/PageHeading";
 import RoutineBuilder from "./_components/RoutineBuilder";
-import prisma from "@/prisma/prisma";
-
-type FavoriteExercise = {
-  exerciseId: string;
-};
-
-type Exercise = {
-  id: string;
-  name: string;
-  category: string;
-};
-
-type WorkoutPlanExercise = {
-  sets: number;
-  reps: number | null;
-  exerciseDuration: number | null;
-  order: number;
-  trackingType: string;
-  Exercise: Exercise;
-};
-
-type ExistingRoutine = {
-  id: string;
-  name: string;
-  notes: string | null;
-  userId: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-  isSystemRoutine: boolean;
-  systemRoutineCategory: string | null;
-  WorkoutPlanExercise: WorkoutPlanExercise[];
-} | null;
-
-let existingRoutine: ExistingRoutine;
+import { fetchRoutine } from "./_components/fetchRoutine";
+import { searchExercises } from "./_components/searchExercises";
 
 export default async function NewRoutinePage({
-  params,
+  params, searchParams,
 }: {
   params: { id: string };
+  searchParams?: {
+    search?: string;
+  };
 }) {
-  const routineId = params.id;
   const { userId }: { userId: string | null } = auth();
 
   if (!userId) {
     throw new Error("You must be signed in to view this page.");
   }
 
-  if (routineId !== "new") {
-    existingRoutine = await prisma.workoutPlan.findUnique({
-      where: { id: routineId },
-      select: {
-        id: true,
-        name: true,
-        notes: true,
-        userId: true,
-        createdAt: true,
-        updatedAt: true,
-        isSystemRoutine: true,
-        systemRoutineCategory: true,
-        WorkoutPlanExercise: {
-          select: {
-            sets: true,
-            reps: true,
-            exerciseDuration: true,
-            order: true,
-            trackingType: true,
-            Exercise: {
-              select: {
-                id: true,
-                name: true,
-                category: true,
-              },
-            },
-          },
-        },
-      },
-    });
-
-    if (!existingRoutine) {
-      throw new Error("Invalid routine ID.");
-    }
-  } else {
-    existingRoutine = null;
+  const routineId = params.id;
+  const existingRoutine = await fetchRoutine(routineId);
+  if (!existingRoutine && routineId !== "new") {
+    return (<p>Routine not found.</p>);
   }
 
-  const favoriteExercises: FavoriteExercise[] =
-    await prisma.favouriteExercise.findMany({
-      where: {
-        userId: userId,
-      },
-      select: {
-        exerciseId: true,
-      },
-    });
+  const search = searchParams?.search || "";
+  const searchResults = await searchExercises(search);
 
   return (
     <>
@@ -108,7 +39,7 @@ export default async function NewRoutinePage({
       <RoutineBuilder
         existingRoutine={existingRoutine}
         routineId={routineId}
-        favoriteExercises={favoriteExercises}
+        searchResults={searchResults}
       />
     </>
   );
