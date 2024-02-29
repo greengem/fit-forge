cf12357f7fb3a09f82465d341a796b3e02873fc1
Green Gem <chris_waitt@me.com>
2024-02-29 21:21:08 +0000

    Add ExerciseAddToRoutineCreatorButton component

---
+++ app/(protected)/exercises/_components/ExerciseAddToRoutineCreatorButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { Button } from "@nextui-org/button";
+import { IconPlus } from "@tabler/icons-react";
+import { handleAddExerciseToRoutine } from "@/server-actions/RoutineServerActions";
+import { toast } from "sonner";
+
+export default function ExerciseAddToRoutineCreatorButton({ exerciseId }: { exerciseId: string }) {
+  const searchParams = useSearchParams();
+  const routineId = searchParams.get("id");
+
+  const handleClick = async () => {
+    if (routineId) {
+      const response = await handleAddExerciseToRoutine({ exerciseId, routineId });
+      if (response.success) {
+        toast.success(response.message);
+      } else {
+        toast.error(response.message);
+      }
+    } else {
+      console.error('routineId is null');
+    }
+  };
+
+  return (
+    <Button isIconOnly onClick={handleClick}>
+      <IconPlus size={20} />
+    </Button>
+  );
+}
