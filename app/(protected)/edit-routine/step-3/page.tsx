9ac7a1d4cf8dffa59dcd19bcbf3d66f3f045bd99
Green Gem <chris_waitt@me.com>
2024-02-29 21:20:53 +0000

    Add new file for Step 3 of edit routine page

---
+++ app/(protected)/edit-routine/step-3/page.tsx
@@ -0,0 +1,47 @@
+import { auth } from "@clerk/nextjs";
+import prisma from "@/prisma/prisma";
+import PageHeading from "@/components/PageHeading/PageHeading";
+import StepProgress from "../_components/StepProgress";
+import RoutineBuilder from "./_components/RoutineBuilder";
+
+export default async function NewRoutineFormStepTwo({
+    searchParams,
+}: {
+  searchParams?: {
+    id?: string;
+  };
+}) {
+    const { userId }: { userId: string | null } = auth();
+
+    if (!userId) {
+      throw new Error("You must be signed in to view this page.");
+    }
+
+    const routineId = searchParams?.id || "";
+
+    const routine = await prisma.workoutPlan.findUnique({
+        where: {
+            id: routineId,
+        },
+        include: {
+            WorkoutPlanExercise: {
+                include: {
+                    Exercise: true,
+                },
+            },
+        }
+    });
+
+    if (!routine) {
+        throw new Error("No Routine found.");
+    }
+
+    return (
+        <>
+            <PageHeading title="New Routine - Step 3" />
+            <h4>{routine?.name}</h4>
+            <StepProgress routineId={routineId} />
+            <RoutineBuilder routine={routine} />
+        </>
+    );
+}
