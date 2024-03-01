400f26469920637032ff9c3725532b24bfbbaea7
Green Gem <chris_waitt@me.com>
2024-03-01 13:20:31 +0000

    Update layoutClass in LayoutWrapper.client.tsx

--- app/(protected)/LayoutWrapper.client.tsx
+++ app/(protected)/LayoutWrapper.client.tsx
@@ -5,7 +5,7 @@ import { useSidebarToggleContext } from '@/contexts/SidebarToggleContext';
 export default function LayoutWrapper({ children } : { children: React.ReactNode }) {
     const { sidebarCollapse } = useSidebarToggleContext();
 
-    const layoutClass = clsx('p-3 md:p-5 ml-0', {
+    const layoutClass = clsx('flex flex-col grow p-3 md:p-5 ml-0', {
       'md:ml-20': sidebarCollapse,
       'md:ml-64': !sidebarCollapse,
     });
