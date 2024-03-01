5de71d631e6e6ea7fcc71ec51de24cbae98a0451
Green Gem <chris_waitt@me.com>
2024-03-01 13:20:47 +0000

    Refactor StepProgress component to use Tabs

--- app/(protected)/edit-routine/_components/StepProgress.tsx
+++ app/(protected)/edit-routine/_components/StepProgress.tsx
@@ -1,32 +1,24 @@
 'use client'
+import {Tabs, Tab} from "@nextui-org/tabs";
 import { usePathname } from "next/navigation";
-import clsx from 'clsx';
-import Link from "next/link";
 
 export default function StepProgress({ routineId } : { routineId: string | null }) {
-    const pathname = usePathname();
+  const pathname = usePathname();
+  const step = pathname.split('/').pop();
 
   return (
-    <div className="flex justify-center mb-3">
-      <div className="grid grid-cols-3 text-center text-xs">
-
-          <div className={clsx("px-4 py-3 rounded-l-xl", {
-            'bg-zinc-700': pathname.includes('step-1'),
-            'bg-zinc-800': !pathname.includes('step-1'),
-          })}>Step 1: Name</div>
-
-          <div className={clsx("px-4 py-3 border-r-2 border-l-2 border-zinc-900", {
-            'bg-zinc-700': pathname.includes('step-2'),
-            'bg-zinc-800 border-zinc-900': !pathname.includes('step-2'),
-          })}>Step 2: Exercises</div>
-
-          <div className={clsx("px-4 py-3 rounded-r-xl", {
-            'bg-zinc-700': pathname.includes('step-3'),
-            'bg-zinc-800': !pathname.includes('step-3'),
-          })}>Step 3: Details</div>
-
-      </div>
-      
+    <div className="flex justify-center mb-5">
+      <Tabs 
+        aria-label="Options" 
+        selectedKey={step} 
+        variant="bordered" 
+        color="primary"
+       
+      >
+        <Tab key="step-1" title="Step 1" href={`/edit-routine/step-1?id=${routineId}`} />
+        <Tab key="step-2" title="Step 2" href={`/edit-routine/step-2?id=${routineId}`} />
+        <Tab key="step-3" title="Step 3" href={`/edit-routine/step-3?id=${routineId}`} />
+      </Tabs>
     </div>
   );
 }
