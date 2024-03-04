3ed4e49430495a3611d82c279e7b01fd8689beca
Green Gem <chris_waitt@me.com>
2024-03-04 11:12:52 +0000

    Add EditActivity component for editing workouts

---
+++ app/(protected)/activity/[id]/page.tsx
@@ -0,0 +1,59 @@
+import { auth } from "@clerk/nextjs";
+import prisma from "@/prisma/prisma";
+import EditWorkout from "./EditWorkout";
+
+export default async function EditActivity({ params } : { params: { id: string } }) {
+    const { userId }: { userId: string | null } = auth();
+    const workoutId = params.id;
+
+    if (!userId) {
+      throw new Error("You must be signed in to view this page.");
+    }
+
+    if (!workoutId) {
+        throw new Error("You must provide a workout id.");
+    }
+
+    const workout = await prisma.workoutLog.findUnique({
+        where: {
+          id: workoutId,
+        },
+        select: {
+          id: true,
+          duration: true,
+          date: true,
+          WorkoutPlan: {
+            select: {
+              name: true,
+            },
+          },
+          exercises: {
+            select: {
+              id: true,
+              exerciseId: true,
+              trackingType: true,
+              Exercise: {
+                select: {
+                  name: true,
+                },
+              },
+              sets: {
+                select: {
+                  weight: true,
+                  reps: true,
+                  exerciseDuration: true,
+                },
+              },
+            },
+          },
+        },
+    });
+
+    if (!workout) {
+        throw new Error("No workout found.");
+    }
+
+    return (
+        <EditWorkout workout={workout} />
+    )
+}
