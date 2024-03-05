479254c0dfd465751b21aa33c2b4fe8cbff3f616
Green Gem <chris_waitt@me.com>
2024-03-05 16:03:56 +0000

    Update SidebarBrand component with new icon and brand name

--- components/Sidebar/SidebarBrand.tsx
+++ components/Sidebar/SidebarBrand.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useSidebarToggleContext } from '@/contexts/SidebarToggleContext';
-import { IconBarbell } from "@tabler/icons-react";
+import { IconBarbell, IconFlame } from "@tabler/icons-react";
 import Link from "next/link";
 import clsx from 'clsx';
 
@@ -11,11 +11,11 @@ export default function SidebarBrand() {
     <div className={clsx("px-5 mb-6", sidebarCollapse ? "flex justify-center" : "")}>
       <Link href="/" className="flex items-center gap-3">
         <div className="flex items-end justify-center w-11 h-[60px] pb-2 bg-primary rounded-b-lg">
-          <IconBarbell stroke={1} size={28} className="text-black" />
+          <IconFlame size={28} className="text-black" />
         </div>
         {!sidebarCollapse &&
           <div className="flex gap-3 items-center pt-[18px]">
-            <p className="text-xl">Tracktive</p>
+            <p className="text-xl">{process.env.NEXT_PUBLIC_BRAND_NAME}</p>
             {/* <Chip color="primary" radius="full">Pro</Chip> */}
           </div>
         }
