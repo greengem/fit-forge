438cfef08aad8e21785bf5c56b3a8ee0325594a4
Green Gem <chris_waitt@me.com>
2024-03-05 18:44:36 +0000

    Refactor sign out functionality in ProfileActions component

--- app/(protected)/profile/_components/ProfileActions.tsx
+++ app/(protected)/profile/_components/ProfileActions.tsx
@@ -1,14 +1,18 @@
 "use client";
-import { SignOutButton } from "@clerk/nextjs";
+import { useClerk } from "@clerk/clerk-react";
+import { useRouter } from 'next/navigation'
 import { Button } from "@nextui-org/button";
 import { IconLogout } from "@tabler/icons-react";
 
 export default function ProfileActions() {
+  const { signOut } = useClerk();
+  const router = useRouter()
+
   return (
-    <SignOutButton>
-      <Button>
+    <div>
+      <Button onClick={() => signOut(() => router.push("/"))}>
         <IconLogout /> Sign out
       </Button>
-    </SignOutButton>
+    </div>
   );
 }
