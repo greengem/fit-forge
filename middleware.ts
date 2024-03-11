d222311152dd1aad7dd80f763f9149f45ec5961c
Green Gem <chris_waitt@me.com>
2024-03-11 15:01:58 +0000

    Update authMiddleware configuration

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ["/"],
-  //ignoredRoutes: ["/"],
+  //publicRoutes: ["/"],
+  ignoredRoutes: ["/"],
 });
  
 export const config = {
