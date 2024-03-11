5d281ac018dea9ecc54e1de347076a77225b66e5
Green Gem <chris_waitt@me.com>
2024-03-11 19:03:26 +0000

    Add trophy icon to Weekly PBs card

--- app/(protected)/dashboard/_components/DashboardCards/DashboardCardWeeklyPbs.tsx
+++ app/(protected)/dashboard/_components/DashboardCards/DashboardCardWeeklyPbs.tsx
@@ -1,6 +1,7 @@
 // DashboardCardWeeklyPbs.tsx
+import { IconTrophy } from "@tabler/icons-react";
 import DashboardCardTemplate from "./DashboardCardTemplate";
 
 export default function DashboardCardWeeklyPbs() {
-  return <DashboardCardTemplate title="Weekly PBs">...</DashboardCardTemplate>;
+  return <DashboardCardTemplate title="Weekly PBs" icon={<IconTrophy className="text-danger" />}>...</DashboardCardTemplate>;
 }
