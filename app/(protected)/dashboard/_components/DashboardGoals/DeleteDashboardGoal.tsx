75e3c9cc7a26c260ff83f2b52f951631b17cf169
Green Gem <chris_waitt@me.com>
2024-03-12 11:15:10 +0000

    Add DeleteDashboardGoal component

---
+++ app/(protected)/dashboard/_components/DashboardGoals/DeleteDashboardGoal.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { handleDeleteUserGoal } from "@/server-actions/UserServerActions";
+import { IconX } from "@tabler/icons-react";
+import { useTransition } from "react"
+
+export default function DeleteDashboardGoal({ id  } : { id: string }) {
+    const [isPending, startTransition] = useTransition();
+
+    const handleClick = () => {
+        if (window.confirm("Are you sure you want to delete this goal?")) {
+            startTransition(() => {
+                handleDeleteUserGoal(id);
+            });
+        }
+    };
+
+    return <button onClick={handleClick} disabled={isPending}><IconX className="text-zinc-500" /></button>;
+}
