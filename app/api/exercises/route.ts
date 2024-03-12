65df5f25667e9157e7be7be5f0c53e8d633f8c4f
Green Gem <chris_waitt@me.com>
2024-03-12 11:15:20 +0000

    Add exercise route with authentication check

---
+++ app/api/exercises/route.ts
@@ -0,0 +1,30 @@
+import { auth } from "@clerk/nextjs";
+import prisma from "@/prisma/prisma";
+
+export async function GET() {
+    try {
+      const { userId } = auth();
+  
+      if (!userId) {
+          throw new Error("You must be signed in to view this page.");
+      }
+  
+      const exercises = await prisma.exercise.findMany({
+        where: {
+            favouritedBy: {
+                some: {
+                    userId: userId
+                }
+            }
+        },
+        select: {
+            id: true,
+            name: true,
+        },
+      });
+  
+      return Response.json(exercises);
+    } catch (error) {
+      return { error: (error as Error).message };
+    }
+  }
