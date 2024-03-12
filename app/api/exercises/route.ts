493d20bdc4244d3f81ea1ef59e4056ec73967fb5
Green Gem <chris_waitt@me.com>
2024-03-12 11:26:11 +0000

    Refactor GET function in exercises route

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -2,29 +2,25 @@ import { auth } from "@clerk/nextjs";
 import prisma from "@/prisma/prisma";
 
 export async function GET() {
-  try {
-    const { userId } = auth();
+  const { userId } = auth();
 
-    if (!userId) {
-      throw new Error("You must be signed in to view this page.");
-    }
+  if (!userId) {
+    throw new Error("You must be signed in to view this page.");
+  }
 
-    const exercises = await prisma.exercise.findMany({
-      where: {
-        favouritedBy: {
-          some: {
-            userId: userId,
-          },
+  const exercises = await prisma.exercise.findMany({
+    where: {
+      favouritedBy: {
+        some: {
+          userId: userId,
         },
       },
-      select: {
-        id: true,
-        name: true,
-      },
-    });
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
 
-    return Response.json(exercises);
-  } catch (error) {
-    return { error: (error as Error).message };
-  }
+  return Response.json(exercises);
 }
