fd6ca55444b07cb71130fac9f62dda1179303f0f
Green Gem <chris_waitt@me.com>
2024-03-12 11:14:45 +0000

    Enable DashboardGoals component

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import DashboardLinks from "./_components/DashboardLinks";
 import DashboardRecentActivity from "./_components/DashboardRecentActivity";
 import DashboardCards from "./_components/DashboardCards/DashboardCards";
 import DashboardCharts from "./_components/DashboardCharts/DashboardCharts";
-//import DashboardGoals from "./_components/DashboardGoals/DashboardGoals";
+import DashboardGoals from "./_components/DashboardGoals/DashboardGoals";
 
 export default function DashboardPage({
   searchParams,
@@ -34,7 +34,7 @@ export default function DashboardPage({
       
       <DashboardLinks />
 
-      {/* <DashboardGoals /> */}
+      <DashboardGoals />
 
       <Suspense fallback={<div>Loading Recent Activity...</div>}>
         <DashboardRecentActivity />
