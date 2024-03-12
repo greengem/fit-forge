68e2a5b2697af8fceaa60ccbd47b95399ad9803e
Green Gem <chris_waitt@me.com>
2024-03-12 11:15:05 +0000

    Refactor DashboardGoalTemplate component

--- app/(protected)/dashboard/_components/DashboardGoals/DashboardGoalTemplate.tsx
+++ app/(protected)/dashboard/_components/DashboardGoals/DashboardGoalTemplate.tsx
@@ -1,22 +1,26 @@
 import { Card, CardHeader, CardBody } from "@nextui-org/card";
-import { IconSettings } from "@tabler/icons-react";
+import DeleteDashboardGoal from "./DeleteDashboardGoal";
 
 export default function DashboardGoalTemplate({
   children,
   title,
   icon,
+  showSettings,
+  id
 }: {
   children: React.ReactNode;
   title: string;
   icon?: React.ReactNode;
+  showSettings?: boolean;
+  id?: string;
 }) {
   return (
-    <Card shadow="none" className="shadow-md">
+    <Card shadow="none" className="shadow-md min-h-[148px]">
       <CardHeader className="px-3 text-xs uppercase block w-full truncate flex justify-between items-center">
         <div className="flex gap-3 items-center">{icon}{title}</div>
-        <IconSettings className="text-zinc-500" />
+        {showSettings && id && <DeleteDashboardGoal id={id} />}
       </CardHeader>
-      <CardBody className="px-3 pt-0 text-primary">
+      <CardBody className="px-3 pt-0 relative">
         <div className="mb-3">{children}</div>
       </CardBody>
     </Card>
